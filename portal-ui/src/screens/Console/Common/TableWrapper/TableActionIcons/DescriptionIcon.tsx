4c203e7edc91156b2433cb2950732b10aa282123
Benjamin Perez <benjamin@bexsoft.net>
2020-05-22 11:02:58 -0500

    Fixed styles in users policy modal

---
+++ portal-ui/src/screens/Console/Common/TableWrapper/TableActionIcons/DescriptionIcon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { IIcon, selected, unSelected } from "./common";
+
+const PencilIcon = ({ active = false }: IIcon) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill={active ? selected : unSelected}
+        d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"
+      ></path>
+    </svg>
+  );
+};
+
+export default PencilIcon;
