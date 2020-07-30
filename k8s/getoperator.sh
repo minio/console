1dabfb4eada843155dd6dddaf252c29d96da2b55
Cesar N <ces.nietor@gmail.com>
2020-07-30 15:21:45 -0700

    Update to minio-operator 3.0.5 (#218)

--- k8s/getoperator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Get's the latest deployment file from MinIO Operator
-curl https://raw.githubusercontent.com/minio/operator/master/minio-operator.yaml > operator-console/base/minio-operator.yaml
