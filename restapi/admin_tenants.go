01f1bee4256e247c3716ff64fe71153c93922b5d
Daniel Valdivia <hola@danielvaldivia.com>
2020-07-30 20:54:21 -0700

    Release v0.3.4

--- restapi/admin_tenants.go
+++ restapi/admin_tenants.go
@@ -690,7 +690,7 @@ func getTenantCreatedResponse(session *models.Principal, params admin_api.Create
 			return nil, err
 		}
 
-		const consoleVersion = "minio/console:v0.3.3"
+		const consoleVersion = "minio/console:v0.3.4"
 		minInst.Spec.Console = &operator.ConsoleConfiguration{
 			Replicas:      2,
 			Image:         consoleVersion,
