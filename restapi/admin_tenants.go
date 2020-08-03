59bf546b4acbe3ac76cc945e91792790d9daa6cb
Minio Trusted <trusted@minio.io>
2020-08-03 09:23:15 -0700

    upgrade to v0.3.5

--- restapi/admin_tenants.go
+++ restapi/admin_tenants.go
@@ -768,7 +768,7 @@ func getTenantCreatedResponse(session *models.Principal, params admin_api.Create
 			return nil, err
 		}
 
-		const consoleVersion = "minio/console:v0.3.4"
+		const consoleVersion = "minio/console:v0.3.5"
 		minInst.Spec.Console = &operator.ConsoleConfiguration{
 			Replicas:      2,
 			Image:         consoleVersion,
