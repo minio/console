c1963c61227cf5a38b2aacfa53bda6db21807082
Minio Trusted <trusted@minio.io>
2020-08-05 11:06:25 -0700

    update to v0.3.7

--- restapi/admin_tenants.go
+++ restapi/admin_tenants.go
@@ -798,7 +798,7 @@ func getTenantCreatedResponse(session *models.Principal, params admin_api.Create
 			return nil, err
 		}
 
-		const consoleVersion = "minio/console:v0.3.6"
+		const consoleVersion = "minio/console:v0.3.7"
 		minInst.Spec.Console = &operator.ConsoleConfiguration{
 			Replicas:      2,
 			Image:         consoleVersion,
