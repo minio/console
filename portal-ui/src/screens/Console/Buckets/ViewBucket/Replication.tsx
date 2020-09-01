52ee3cf6a62d2e5b0ea6e168385e925e71500ed7
Benjamin Perez <benjamin@bexsoft.net>
2020-08-31 23:06:17 -0500

    Added replication modals

--- portal-ui/src/screens/Console/Buckets/ViewBucket/Replication.tsx
+++ portal-ui/src/screens/Console/Buckets/ViewBucket/Replication.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import get from "lodash/get";
 import ModalWrapper from "../../Common/ModalWrapper/ModalWrapper";
 import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
 import { modalBasic } from "../../Common/FormComponents/common/styleLibrary";
@@ -7,6 +8,12 @@ import Typography from "@material-ui/core/Typography";
 import InputBoxWrapper from "../../Common/FormComponents/InputBoxWrapper/InputBoxWrapper";
 import SelectWrapper from "../../Common/FormComponents/SelectWrapper/SelectWrapper";
 import { Button, LinearProgress } from "@material-ui/core";
+import api from "../../../../common/api";
+import {
+  IRemoteBucket,
+  IRemoteBucketsResponse,
+} from "../../RemoteBuckets/types";
+import RemoteBucketsList from "../../RemoteBuckets/RemoteBuckets";
 
 interface IReplicationModal {
   open: boolean;
@@ -39,6 +46,7 @@ const Replication = ({
   classes,
 }: IReplicationModal) => {
   const [addError, setAddError] = useState("");
+  const [loadingForm, setLoadingForm] = useState(true);
   const [addLoading, setAddLoading] = useState(false);
   const [remoteURL, setRemoteURL] = useState("");
   const [source, setSource] = useState("");
@@ -52,8 +60,48 @@ const Replication = ({
     }
   }, [addLoading]);
 
+  useEffect(() => {
+    if (loadingForm) {
+      getARNValues();
+    }
+  });
+
   const addRecord = () => {
-    setAddLoading(false);
+    const replicationInfo = {
+      remoteURL,
+      source,
+      target,
+      ARN,
+    };
+
+    api
+      .invoke("PUT", "/api/v1/replication", replicationInfo)
+      .then((res) => {
+        setAddLoading(false);
+        setAddError("");
+        closeModalAndRefresh();
+      })
+      .catch((err) => {
+        setAddLoading(false);
+        setAddError(err);
+      });
+  };
+
+  const getARNValues = () => {
+    api
+      .invoke("GET", "/api/v1/remote-buckets")
+      .then((res: any) => {
+        const remoteBuckets = get(res, "buckets", []);
+
+        const remoteARNS = remoteBuckets.map((itemRemote: IRemoteBucket) => {
+          return { label: itemRemote.remoteARN, value: itemRemote.remoteARN };
+        });
+        setLoadingForm(false);
+        setARNValues(remoteARNS);
+      })
+      .catch((err) => {
+        setLoadingForm(false);
+      });
   };
 
   return (
@@ -73,81 +121,90 @@ const Replication = ({
           setAddLoading(true);
         }}
       >
-        <Grid container>
-          <Grid item xs={12} className={classes.formScrollable}>
-            {addError !== "" && (
-              <Grid item xs={12}>
-                <Typography
-                  component="p"
-                  variant="body1"
-                  className={classes.errorBlock}
-                >
-                  {addError}
-                </Typography>
-              </Grid>
-            )}
-            <Grid item xs={12}>
-              <InputBoxWrapper
-                id="remote-url"
-                name="remote-url"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setRemoteURL(e.target.value);
-                }}
-                label="Remote URL"
-                value={remoteURL}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <InputBoxWrapper
-                id="source"
-                name="source"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setSource(e.target.value);
-                }}
-                label="Source"
-                value={source}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <InputBoxWrapper
-                id="target"
-                name="target"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setTarget(e.target.value);
-                }}
-                label="Target"
-                value={target}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <SelectWrapper
-                onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
-                  setARN(e.target.value as string);
-                }}
-                id="arn"
-                name="arn"
-                label={"ARN"}
-                value={ARN}
-                options={arnValues}
-              />
-            </Grid>
-          </Grid>
-          <Grid item xs={12} className={classes.buttonContainer}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              disabled={addLoading}
-            >
-              Save
-            </Button>
-          </Grid>
-          {addLoading && (
+        {loadingForm && (
+          <Grid container>
             <Grid item xs={12}>
               <LinearProgress />
             </Grid>
-          )}
-        </Grid>
+          </Grid>
+        )}
+        {!loadingForm && (
+          <Grid container>
+            <Grid item xs={12} className={classes.formScrollable}>
+              {addError !== "" && (
+                <Grid item xs={12}>
+                  <Typography
+                    component="p"
+                    variant="body1"
+                    className={classes.errorBlock}
+                  >
+                    {addError}
+                  </Typography>
+                </Grid>
+              )}
+              <Grid item xs={12}>
+                <InputBoxWrapper
+                  id="remote-url"
+                  name="remote-url"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setRemoteURL(e.target.value);
+                  }}
+                  label="Remote URL"
+                  value={remoteURL}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <InputBoxWrapper
+                  id="source"
+                  name="source"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setSource(e.target.value);
+                  }}
+                  label="Source"
+                  value={source}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <InputBoxWrapper
+                  id="target"
+                  name="target"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setTarget(e.target.value);
+                  }}
+                  label="Target"
+                  value={target}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <SelectWrapper
+                  onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
+                    setARN(e.target.value as string);
+                  }}
+                  id="arn"
+                  name="arn"
+                  label={"ARN"}
+                  value={ARN}
+                  options={arnValues}
+                />
+              </Grid>
+            </Grid>
+            <Grid item xs={12} className={classes.buttonContainer}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={addLoading}
+              >
+                Save
+              </Button>
+            </Grid>
+            {addLoading && (
+              <Grid item xs={12}>
+                <LinearProgress />
+              </Grid>
+            )}
+          </Grid>
+        )}
       </form>
     </ModalWrapper>
   );
