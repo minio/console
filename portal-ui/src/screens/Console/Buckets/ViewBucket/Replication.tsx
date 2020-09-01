728d7e819fad6860fc5279714341b742155e2b5f
Benjamin Perez <benjamin@bexsoft.net>
2020-08-31 22:41:11 -0500

    Add replication modal

--- portal-ui/src/screens/Console/Buckets/ViewBucket/Replication.tsx
+++ portal-ui/src/screens/Console/Buckets/ViewBucket/Replication.tsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ModalWrapper from "../../Common/ModalWrapper/ModalWrapper";
 import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
 import { modalBasic } from "../../Common/FormComponents/common/styleLibrary";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import InputBoxWrapper from "../../Common/FormComponents/InputBoxWrapper/InputBoxWrapper";
+import SelectWrapper from "../../Common/FormComponents/SelectWrapper/SelectWrapper";
+import { Button, LinearProgress } from "@material-ui/core";
 
 interface IReplicationModal {
   open: boolean;
@@ -34,6 +39,23 @@ const Replication = ({
   classes,
 }: IReplicationModal) => {
   const [addError, setAddError] = useState("");
+  const [addLoading, setAddLoading] = useState(false);
+  const [remoteURL, setRemoteURL] = useState("");
+  const [source, setSource] = useState("");
+  const [target, setTarget] = useState("");
+  const [ARN, setARN] = useState("");
+  const [arnValues, setARNValues] = useState([]);
+
+  useEffect(() => {
+    if (addLoading) {
+      addRecord();
+    }
+  }, [addLoading]);
+
+  const addRecord = () => {
+    setAddLoading(false);
+  };
+
   return (
     <ModalWrapper
       modalOpen={open}
@@ -43,7 +65,90 @@ const Replication = ({
       }}
       title="Set Bucket Replication"
     >
-      Bucket Replication
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          setAddLoading(true);
+        }}
+      >
+        <Grid container>
+          <Grid item xs={12} className={classes.formScrollable}>
+            {addError !== "" && (
+              <Grid item xs={12}>
+                <Typography
+                  component="p"
+                  variant="body1"
+                  className={classes.errorBlock}
+                >
+                  {addError}
+                </Typography>
+              </Grid>
+            )}
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="remote-url"
+                name="remote-url"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setRemoteURL(e.target.value);
+                }}
+                label="Remote URL"
+                value={remoteURL}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="source"
+                name="source"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setSource(e.target.value);
+                }}
+                label="Source"
+                value={source}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="target"
+                name="target"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setTarget(e.target.value);
+                }}
+                label="Target"
+                value={target}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <SelectWrapper
+                onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
+                  setARN(e.target.value as string);
+                }}
+                id="arn"
+                name="arn"
+                label={"ARN"}
+                value={ARN}
+                options={arnValues}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} className={classes.buttonContainer}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={addLoading}
+            >
+              Save
+            </Button>
+          </Grid>
+          {addLoading && (
+            <Grid item xs={12}>
+              <LinearProgress />
+            </Grid>
+          )}
+        </Grid>
+      </form>
     </ModalWrapper>
   );
 };
--- portal-ui/src/screens/Console/RemoteBuckets/AddRemoteBucket.tsx
+++ portal-ui/src/screens/Console/RemoteBuckets/AddRemoteBucket.tsx
@@ -14,248 +14,247 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import {Button, LinearProgress} from "@material-ui/core";
-import {createStyles, Theme, withStyles} from "@material-ui/core/styles";
-import {modalBasic} from "../Common/FormComponents/common/styleLibrary";
+import { Button, LinearProgress } from "@material-ui/core";
+import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
+import { modalBasic } from "../Common/FormComponents/common/styleLibrary";
 import api from "../../../common/api";
 import ModalWrapper from "../Common/ModalWrapper/ModalWrapper";
 import InputBoxWrapper from "../Common/FormComponents/InputBoxWrapper/InputBoxWrapper";
 import SelectWrapper from "../Common/FormComponents/SelectWrapper/SelectWrapper";
 
 const styles = (theme: Theme) =>
-    createStyles({
-        errorBlock: {
-            color: "red",
-        },
-        buttonContainer: {
-            textAlign: "right",
-        },
-        ...modalBasic,
-    });
+  createStyles({
+    errorBlock: {
+      color: "red",
+    },
+    buttonContainer: {
+      textAlign: "right",
+    },
+    ...modalBasic,
+  });
 
 interface IAddBucketProps {
-    classes: any;
-    open: boolean;
-    closeModalAndRefresh: () => void;
+  classes: any;
+  open: boolean;
+  closeModalAndRefresh: () => void;
 }
 
 const AddRemoteBucket = ({
-                             classes,
-                             open,
-                             closeModalAndRefresh,
-                         }: IAddBucketProps) => {
-    const [addLoading, setAddLoading] = useState(false);
-    const [addError, setAddError] = useState("");
-    const [bucketName, setBucketName] = useState("");
-    const [accessKey, setAccessKey] = useState("");
-    const [secretKey, setSecretKey] = useState("");
-    const [sourceBucket, setSourceBucket] = useState("");
-    const [targetURL, setTargetURL] = useState("");
-    const [targetBucket, setTargetBucket] = useState("");
-    const [remoteARN, setRemoteARN] = useState("");
-    const [status, setStatus] = useState("");
-    const [service, setService] = useState("");
-    const [arnList, setArnList] = useState([]);
+  classes,
+  open,
+  closeModalAndRefresh,
+}: IAddBucketProps) => {
+  const [addLoading, setAddLoading] = useState(false);
+  const [addError, setAddError] = useState("");
+  const [bucketName, setBucketName] = useState("");
+  const [accessKey, setAccessKey] = useState("");
+  const [secretKey, setSecretKey] = useState("");
+  const [sourceBucket, setSourceBucket] = useState("");
+  const [targetURL, setTargetURL] = useState("");
+  const [targetBucket, setTargetBucket] = useState("");
+  const [remoteARN, setRemoteARN] = useState("");
+  const [status, setStatus] = useState("");
+  const [service, setService] = useState("");
+  const [arnList, setArnList] = useState([]);
 
-    useEffect(() => {
-        if (addLoading) {
-            addRecord();
-        }
-    }, [addLoading]);
-
-    const addRecord = () => {
-        const remoteBucketInfo = {
-            name: bucketName,
-            accessKey: accessKey,
-            secretKey: secretKey,
-            sourceBucket: sourceBucket,
-            targetURL: targetURL,
-            targetBucket: targetBucket,
-            remoteARN: remoteARN,
-            status,
-            service,
-        };
-
-        api
-            .invoke("POST", "/api/v1/remote-buckets", remoteBucketInfo)
-            .then((res) => {
-                setAddLoading(false);
-                setAddError("");
-                closeModalAndRefresh();
-            })
-            .catch((err) => {
-                setAddLoading(false);
-                setAddError(err);
-            });
+  useEffect(() => {
+    if (addLoading) {
+      addRecord();
     }
+  }, [addLoading]);
 
+  const addRecord = () => {
+    const remoteBucketInfo = {
+      name: bucketName,
+      accessKey: accessKey,
+      secretKey: secretKey,
+      sourceBucket: sourceBucket,
+      targetURL: targetURL,
+      targetBucket: targetBucket,
+      remoteARN: remoteARN,
+      status,
+      service,
+    };
 
-    const arnValues = arnList.map((arnConstant) => ({
-        label: arnConstant,
-        value: arnConstant,
-    }));
+    api
+      .invoke("POST", "/api/v1/remote-buckets", remoteBucketInfo)
+      .then((res) => {
+        setAddLoading(false);
+        setAddError("");
+        closeModalAndRefresh();
+      })
+      .catch((err) => {
+        setAddLoading(false);
+        setAddError(err);
+      });
+  };
 
-    const serviceOptions = [
-        {label: "None", value: ""},
-        {label: "Replication", value: "replication"},
-    ];
+  const arnValues = arnList.map((arnConstant) => ({
+    label: arnConstant,
+    value: arnConstant,
+  }));
 
-    return (
-        <ModalWrapper
-            title="Create Remote Bucket"
-            modalOpen={open}
-            onClose={() => {
-                setAddError("");
-                closeModalAndRefresh();
-            }}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-        >
-            <form
-                noValidate
-                autoComplete="off"
-                onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-                    e.preventDefault();
-                    addRecord();
+  const serviceOptions = [
+    { label: "None", value: "" },
+    { label: "Replication", value: "replication" },
+  ];
+
+  return (
+    <ModalWrapper
+      title="Create Remote Bucket"
+      modalOpen={open}
+      onClose={() => {
+        setAddError("");
+        closeModalAndRefresh();
+      }}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          addRecord();
+        }}
+      >
+        <Grid container>
+          <Grid item xs={12} className={classes.formScrollable}>
+            {addError !== "" && (
+              <Grid item xs={12}>
+                <Typography
+                  component="p"
+                  variant="body1"
+                  className={classes.errorBlock}
+                >
+                  {addError}
+                </Typography>
+              </Grid>
+            )}
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="bucket-name"
+                name="bucket-name"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setBucketName(e.target.value);
                 }}
+                label="Remote Bucket Name"
+                value={bucketName}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="accessKey"
+                name="accessKey"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setAccessKey(e.target.value);
+                }}
+                label="Access Key"
+                value={accessKey}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="secretKey"
+                name="secretKey"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setSecretKey(e.target.value);
+                }}
+                label="Secret Key"
+                value={secretKey}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="sourceBucket"
+                name="sourceBucket"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setSourceBucket(e.target.value);
+                }}
+                label="Source Bucket"
+                value={sourceBucket}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="targetURL"
+                name="targetURL"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setTargetURL(e.target.value);
+                }}
+                label="Target URL"
+                value={targetURL}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="targetBucket"
+                name="targetBucket"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setTargetBucket(e.target.value);
+                }}
+                label="Target Bucket"
+                value={targetBucket}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <SelectWrapper
+                onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
+                  setRemoteARN(e.target.value as string);
+                }}
+                id="remoteARN"
+                name="remoteARN"
+                label={"Remote ARN"}
+                value={remoteARN}
+                options={arnValues}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="status"
+                name="status"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setStatus(e.target.value);
+                }}
+                label="Status"
+                value={status}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <SelectWrapper
+                onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
+                  setService(e.target.value as string);
+                }}
+                id="service"
+                name="service"
+                label={"Service"}
+                value={service}
+                options={serviceOptions}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} className={classes.buttonContainer}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={addLoading}
             >
-                <Grid container>
-                    <Grid item xs={12} className={classes.formScrollable}>
-                        {addError !== "" && (
-                            <Grid item xs={12}>
-                                <Typography
-                                    component="p"
-                                    variant="body1"
-                                    className={classes.errorBlock}
-                                >
-                                    {addError}
-                                </Typography>
-                            </Grid>
-                        )}
-                        <Grid item xs={12}>
-                            <InputBoxWrapper
-                                id="bucket-name"
-                                name="bucket-name"
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setBucketName(e.target.value);
-                                }}
-                                label="Remote Bucket Name"
-                                value={bucketName}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <InputBoxWrapper
-                                id="accessKey"
-                                name="accessKey"
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setAccessKey(e.target.value);
-                                }}
-                                label="Access Key"
-                                value={accessKey}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <InputBoxWrapper
-                                id="secretKey"
-                                name="secretKey"
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setSecretKey(e.target.value);
-                                }}
-                                label="Secret Key"
-                                value={secretKey}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <InputBoxWrapper
-                                id="sourceBucket"
-                                name="sourceBucket"
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setSourceBucket(e.target.value);
-                                }}
-                                label="Source Bucket"
-                                value={sourceBucket}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <InputBoxWrapper
-                                id="targetURL"
-                                name="targetURL"
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setTargetURL(e.target.value);
-                                }}
-                                label="Target URL"
-                                value={targetURL}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <InputBoxWrapper
-                                id="targetBucket"
-                                name="targetBucket"
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setTargetBucket(e.target.value);
-                                }}
-                                label="Target Bucket"
-                                value={targetBucket}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <SelectWrapper
-                                onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
-                                    setRemoteARN(e.target.value as string);
-                                }}
-                                id="remoteARN"
-                                name="remoteARN"
-                                label={"Remote ARN"}
-                                value={remoteARN}
-                                options={arnValues}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <InputBoxWrapper
-                                id="status"
-                                name="status"
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setStatus(e.target.value);
-                                }}
-                                label="Status"
-                                value={status}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <SelectWrapper
-                                onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
-                                    setService(e.target.value as string);
-                                }}
-                                id="service"
-                                name="service"
-                                label={"Service"}
-                                value={service}
-                                options={serviceOptions}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} className={classes.buttonContainer}>
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                            disabled={addLoading}
-                        >
-                            Save
-                        </Button>
-                    </Grid>
-                    {addLoading && (
-                        <Grid item xs={12}>
-                            <LinearProgress/>
-                        </Grid>
-                    )}
-                </Grid>
-            </form>
-        </ModalWrapper>
-    );
-}
+              Save
+            </Button>
+          </Grid>
+          {addLoading && (
+            <Grid item xs={12}>
+              <LinearProgress />
+            </Grid>
+          )}
+        </Grid>
+      </form>
+    </ModalWrapper>
+  );
+};
 
 export default withStyles(styles)(AddRemoteBucket);
