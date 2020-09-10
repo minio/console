3ea3f20346a125d37fd75ba33c601aadc693b73b
Lenin Alevski <alevsk.8772@gmail.com>
2020-09-10 14:22:39 -0700

    stringSort and usersSort test functions

--- portal-ui/src/utils/sortFunctions.test.js
+++ portal-ui/src/utils/sortFunctions.test.js
@@ -1,6 +1,76 @@
-import {policySort} from './sortFunctions';
+import {policySort, usersSort, stringSort} from './sortFunctions';
 
-it('policy sort', () => {
+it('usersSort function', () => {
+  const testCases = [
+    {
+      args: {
+        users: [
+          {
+            accessKey: "user",
+          },
+          {
+            accessKey: "admin",
+          },
+          {
+            accessKey: "operator",
+          },
+          {
+            accessKey: "superadmin",
+          },
+          {
+            accessKey: "partner",
+          },
+        ]
+      },
+      expectedResult:  [{"accessKey": "admin"}, {"accessKey": "operator"}, {"accessKey": "partner"}, {"accessKey": "superadmin"}, {"accessKey": "user"}],
+    },
+    {
+      args: {
+        users: [
+          {
+            accessKey: "user",
+          },
+          {
+            accessKey: "admin",
+          },
+          {
+            accessKey: "operator",
+          },
+          {
+            accessKey: "",
+          },
+        ]
+      },
+      expectedResult:  [{"accessKey": ""}, {"accessKey": "admin"}, {"accessKey": "operator"}, {"accessKey": "user"}],
+    },
+    {
+      args: {
+        users: [
+          {
+            accessKey: "user",
+          },
+          {
+            accessKey: "admin",
+          },
+          {
+            accessKey: "operator",
+          },
+          {
+            accessKey: null,
+          },
+        ]
+      },
+      expectedResult:  [{"accessKey": "admin"}, {"accessKey": "operator"}, {"accessKey": "user"}, {"accessKey": null}],
+    }
+  ];
+  testCases.forEach((test) => {
+    const users = test.args.users;
+    const result = test.expectedResult;
+    expect(users.sort(usersSort)).toEqual(result);
+  })
+});
+
+it('policySort function', () => {
   const testCases = [
     {
       args: {
@@ -67,5 +137,33 @@ it('policy sort', () => {
     const policies = test.args.policies;
     const result = test.expectedResult;
     expect(policies.sort(policySort)).toEqual(result);
+  })
+});
+
+it('stringSort function', () => {
+  const testCases = [
+    {
+      args: {
+        strings: ["user","admin","operator","superadmin"]
+      },
+      expectedResult:  ["admin","operator","superadmin","user"],
+    },
+    {
+      args: {
+        strings: ["user","admin","operator",""],
+      },
+      expectedResult:  ["","admin","operator","user"],
+    },
+    {
+      args: {
+        strings: ["user","admin","operator",null],
+      },
+      expectedResult:  ["admin","operator","user",null],
+    }
+  ];
+  testCases.forEach((test) => {
+    const strings = test.args.strings;
+    const result = test.expectedResult;
+    expect(strings.sort(stringSort)).toEqual(result);
   })
 });
