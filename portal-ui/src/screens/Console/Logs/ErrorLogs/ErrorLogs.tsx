2abf07844d02c9f676a1c82c9097d5c1df31d175
Benjamin Perez <benjamin@bexsoft.net>
2021-01-05 17:50:29 -0600

    Removed typo

--- portal-ui/src/screens/Console/Logs/ErrorLogs/ErrorLogs.tsx
+++ portal-ui/src/screens/Console/Logs/ErrorLogs/ErrorLogs.tsx
@@ -117,7 +117,6 @@ const ErrorLogs = ({
         console.log("connection closed by server");
       };
       return () => {
-        timeFromDate;
         c.close(1000);
         clearInterval(interval);
         console.log("closing websockets");
