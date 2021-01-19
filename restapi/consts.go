6a96ec8177b229e1ad4c7cbb9ef34305fcce4dba
Minio Trusted <trusted@minio.io>
2021-01-19 02:08:37 -0800

    use .Tag instead of .Version to have consistent naming

--- restapi/consts.go
+++ restapi/consts.go
@@ -63,7 +63,7 @@ const (
 // Image versions
 const (
 	KESImageVersion            = "minio/kes:v0.13.1"
-	ConsoleImageDefaultVersion = "minio/console:v0.5.0"
+	ConsoleImageDefaultVersion = "minio/console:v0.5.1"
 )
 
 // K8s
