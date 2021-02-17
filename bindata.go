1bdc8fd223449d2ea0c1663c750501185cdebb44
jinapurapu <jill.inapurapu@gmail.com>
2021-02-17 11:14:14 -0800

    Added refresh tenant functionality

---
+++ bindata.go
@@ -0,0 +1,10 @@
+
+func assetFS() *assetfs.AssetFS {
+	assetInfo := func(path string) (os.FileInfo, error) {
+		return os.Stat(path)
+	}
+	for k := range _bintree.Children {
+		return &assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: assetInfo, Prefix: k}
+	}
+	panic("unreachable")
+}
