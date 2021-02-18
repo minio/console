2a5c1afbdf53d0c3d86b039a928da8c41a1bb3a5
Minio Trusted <trusted@minio.io>
2021-02-18 12:12:03 -0800

    release console version v0.6.0

--- restapi/admin_tenants_test.go
+++ restapi/admin_tenants_test.go
@@ -1028,7 +1028,7 @@ func Test_UpdateTenantAction(t *testing.T) {
 				},
 				params: admin_api.UpdateTenantParams{
 					Body: &models.UpdateTenantRequest{
-						ConsoleImage: "minio/console:v0.5.2",
+						ConsoleImage: "minio/console:v0.6.0",
 					},
 				},
 			},
--- restapi/consts.go
+++ restapi/consts.go
@@ -63,7 +63,7 @@ const (
 // Image versions
 const (
 	KESImageVersion            = "minio/kes:v0.13.4"
-	ConsoleImageDefaultVersion = "minio/console:v0.5.2"
+	ConsoleImageDefaultVersion = "minio/console:v0.6.0"
 )
 
 // K8s
