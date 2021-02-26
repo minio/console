8a4139c8e721b9b487fda934ef16e114a48748ce
Minio Trusted <trusted@minio.io>
2021-02-26 13:01:28 -0800

    update to v0.6.2

--- restapi/consts.go
+++ restapi/consts.go
@@ -63,7 +63,7 @@ const (
 // Image versions
 const (
 	KESImageVersion            = "minio/kes:v0.13.4"
-	ConsoleImageDefaultVersion = "minio/console:v0.6.1"
+	ConsoleImageDefaultVersion = "minio/console:v0.6.2"
 )
 
 // K8s
