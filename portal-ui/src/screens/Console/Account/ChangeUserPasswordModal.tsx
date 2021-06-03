a9e82eb9090e362c550539336e974b53bcc7d700
jinapurapu <65002498+jinapurapu@users.noreply.github.com>
2021-06-03 15:32:22 -0700

    #616 added modal on icon click (#747)
    * Added refresh tenant functionality
    
    add icon to Users page to change password
    
    commit work to date to github for ongoing use
    
    add modal with fields for current and new password on icon click
    
    missing swagger files
    
    remove unneeded files
    
    move changeUserPassword to admin_api, remove field for current password,
    include selected user
    
    Please enter the commit message for your changes. Lines starting
    
    added missing js files
    
    asset and function signature
    
    formatting changes
    
    * fixed lint
    
    * removed Current Password field, returned Groups label, added
    selectedUser to modal text
    
    * disabled save button if newPasssword and reNewPassword don't match, removed commented out code
    
    * Added refresh tenant functionality
    
    add icon to Users page to change password
    
    commit work to date to github for ongoing use
    
    add modal with fields for current and new password on icon click
    
    missing swagger files
    
    remove unneeded files
    
    move changeUserPassword to admin_api, remove field for current password,
    include selected user
    
    Please enter the commit message for your changes. Lines starting
    
    added missing js files
    
    asset and function signature
    
    formatting changes
    
    * removed Current Password field, returned Groups label, added
    selectedUser to modal text
    
    * changed Swagger parameters to camel case
    
    Co-authored-by: Jill <jill@minio.io>

---
+++ portal-ui/src/screens/Console/Account/ChangeUserPasswordModal.tsx
@@ -0,0 +1,167 @@
+// This file is part of MinIO Console Server
+// Copyright (c) 2021 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
+import ModalWrapper from "../Common/ModalWrapper/ModalWrapper";
+import Grid from "@material-ui/core/Grid";
+import InputBoxWrapper from "../Common/FormComponents/InputBoxWrapper/InputBoxWrapper";
+import { Button, LinearProgress } from "@material-ui/core";
+import {
+  actionsTray,
+  containerForHeader,
+  modalBasic,
+} from "../Common/FormComponents/common/styleLibrary";
+import { ChangeUserPasswordRequest } from "../Buckets/types";
+import api from "../../../common/api";
+import { setModalErrorSnackMessage } from "../../../actions";
+import { User, UsersList } from "../Users/types";
+
+const styles = (theme: Theme) =>
+  createStyles({
+    buttonContainer: {
+      textAlign: "right",
+    },
+    ...actionsTray,
+    ...modalBasic,
+    ...containerForHeader(theme.spacing(4)),
+  });
+
+interface IChangeUserPasswordProps {
+  classes: any;
+  open: boolean;
+  selectedUser: User | null;
+  closeModal: () => void;
+  setModalErrorSnackMessage: typeof setModalErrorSnackMessage;
+}
+
+const ChangeUserPassword = ({
+  classes,
+  open,
+  selectedUser,
+  closeModal,
+  setModalErrorSnackMessage,
+}: IChangeUserPasswordProps) => {
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [reNewPassword, setReNewPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const changeUserPassword = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+
+    let request: ChangeUserPasswordRequest = {
+      selectedUser: String(selectedUser?.accessKey),
+      newSecretKey: newPassword,
+    };
+
+    api
+      .invoke("POST", "/api/v1/account/change-user-password", request)
+      .then((res) => {
+        setLoading(false);
+        setNewPassword("");
+        setReNewPassword("");
+        closeModal();
+      })
+      .catch((err) => {
+        setLoading(false);
+        setNewPassword("");
+        setReNewPassword("");
+        setModalErrorSnackMessage(err);
+      });
+  };
+
+  return open ? (
+    <ModalWrapper
+      title="Change User Password"
+      modalOpen={open}
+      onClose={() => {
+        setNewPassword("");
+        setReNewPassword("");
+        closeModal();
+      }}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          changeUserPassword(e);
+        }}
+      >
+        <Grid container>
+          <Grid item xs={12} className={classes.formScrollable}>
+            <h3>Change password for {selectedUser?.accessKey}</h3>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="new-password"
+                name="new-password"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setNewPassword(event.target.value);
+                }}
+                label="New Password"
+                type="password"
+                value={newPassword}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputBoxWrapper
+                id="re-new-password"
+                name="re-new-password"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setReNewPassword(event.target.value);
+                }}
+                label="Type New Password Again"
+                type="password"
+                value={reNewPassword}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} className={classes.buttonContainer}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={
+                loading ||
+                !(reNewPassword.length > 0 && newPassword == reNewPassword)
+              }
+            >
+              Save
+            </Button>
+          </Grid>
+          {loading && (
+            <Grid item xs={12}>
+              <LinearProgress />
+            </Grid>
+          )}
+        </Grid>
+      </form>
+    </ModalWrapper>
+  ) : null;
+};
+
+const connector = connect(null, {
+  setModalErrorSnackMessage,
+});
+
+export default withStyles(styles)(connector(ChangeUserPassword));
