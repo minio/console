2d6b5ecbc6350e8893476a7e5e916c10dc373f8e
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2021-06-17 19:12:52 -0700

    Re-organize Pod Details (#821)
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

---
+++ portal-ui/src/screens/Console/Tenants/TenantDetails/pods/PodEvents.tsx
@@ -0,0 +1,120 @@
+// This file is part of MinIO Console Server
+// Copyright (c) 2021 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
+import {
+  actionsTray,
+  buttonsStyles,
+  hrClass,
+  searchField,
+} from "../../../Common/FormComponents/common/styleLibrary";
+import Grid from "@material-ui/core/Grid";
+import TableWrapper from "../../../Common/TableWrapper/TableWrapper";
+import api from "../../../../../common/api";
+import { IEvent } from "../../ListTenants/types";
+import { setErrorSnackMessage } from "../../../../../actions";
+import { niceDays } from "../../../../../common/utils";
+
+interface IPodEventsProps {
+  classes: any;
+  tenant: string;
+  namespace: string;
+  podName: string;
+  propLoading: boolean;
+  setErrorSnackMessage: typeof setErrorSnackMessage;
+}
+
+const styles = (theme: Theme) =>
+  createStyles({
+    ...actionsTray,
+    ...buttonsStyles,
+    ...searchField,
+    ...hrClass,
+    actionsTray: {
+      ...actionsTray.actionsTray,
+      padding: "15px 0 0",
+    },
+  });
+
+const PodEvents = ({
+  classes,
+  tenant,
+  namespace,
+  podName,
+  propLoading,
+  setErrorSnackMessage,
+}: IPodEventsProps) => {
+  const [event, setEvent] = useState<IEvent[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (propLoading) {
+      setLoading(true);
+    }
+  }, [propLoading]);
+
+  useEffect(() => {
+    if (loading) {
+      api
+        .invoke(
+          "GET",
+          `/api/v1/namespaces/${namespace}/tenants/${tenant}/pods/${podName}/events`
+        )
+        .then((res: IEvent[]) => {
+          for (let i = 0; i < res.length; i++) {
+            let currentTime = (Date.now() / 1000) | 0;
+
+            res[i].seen = niceDays((currentTime - res[i].last_seen).toString());
+          }
+          setEvent(res);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setErrorSnackMessage(err);
+          setLoading(false);
+        });
+    }
+  }, [loading, podName, namespace, tenant, setErrorSnackMessage]);
+
+  return (
+    <React.Fragment>
+      <Grid item xs={12} className={classes.actionsTray}>
+        <TableWrapper
+          itemActions={[]}
+          columns={[
+            { label: "Namespace", elementKey: "namespace" },
+            { label: "Last Seen", elementKey: "seen" },
+            { label: "Message", elementKey: "message" },
+            { label: "Event Type", elementKey: "event_type" },
+            { label: "Reason", elementKey: "reason" },
+          ]}
+          isLoading={loading}
+          records={event}
+          entityName="Events"
+          idField="event"
+        />
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+const connector = connect(null, {
+  setErrorSnackMessage,
+});
+
+export default withStyles(styles)(connector(PodEvents));
---
+++ portal-ui/src/screens/Console/Tenants/TenantDetails/pods/PodLogs.tsx
@@ -0,0 +1,197 @@
+// This file is part of MinIO Console Server
+// Copyright (c) 2021 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
+import {
+  actionsTray,
+  buttonsStyles,
+  containerForHeader,
+  searchField,
+} from "../../../Common/FormComponents/common/styleLibrary";
+import Grid from "@material-ui/core/Grid";
+import { TextField } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import api from "../../../../../common/api";
+import { setErrorSnackMessage } from "../../../../../actions";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+
+interface IPodLogsProps {
+  classes: any;
+  tenant: string;
+  namespace: string;
+  podName: string;
+  propLoading: boolean;
+  setErrorSnackMessage: typeof setErrorSnackMessage;
+}
+
+const styles = (theme: Theme) =>
+  createStyles({
+    logList: {
+      background: "#fff",
+      minHeight: 400,
+      height: "calc(100vh - 304px)",
+      overflow: "auto",
+      fontSize: 13,
+      padding: "25px 45px 0",
+      border: "1px solid #EAEDEE",
+      borderRadius: 4,
+    },
+    ...buttonsStyles,
+    ...searchField,
+    actionsTray: {
+      ...actionsTray.actionsTray,
+      padding: "15px 0 0",
+    },
+    logerror: {
+      color: "#A52A2A",
+    },
+    logerror_tab: {
+      color: "#A52A2A",
+      paddingLeft: 25,
+    },
+    ansidefault: {
+      color: "#000",
+    },
+    highlight: {
+      "& span": {
+        backgroundColor: "#082F5238",
+      },
+    },
+    ...containerForHeader(theme.spacing(4)),
+  });
+
+const PodLogs = ({
+  classes,
+  tenant,
+  namespace,
+  podName,
+  propLoading,
+  setErrorSnackMessage,
+}: IPodLogsProps) => {
+  const [highlight, setHighlight] = useState<string>("");
+  const [logLines, setLogLines] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (propLoading) {
+      setLoading(true);
+    }
+  }, [propLoading]);
+
+  const renderLog = (logMessage: string, index: number) => {
+    // remove any non ascii characters, exclude any control codes
+    logMessage = logMessage.replace(/([^\x20-\x7F])/g, "");
+
+    // regex for terminal colors like e.g. `[31;4m `
+    const tColorRegex = /((\[[0-9;]+m))/g;
+
+    // get substring if there was a match for to split what
+    // is going to be colored and what not, here we add color
+    // only to the first match.
+    let substr = logMessage.replace(tColorRegex, "");
+
+    // in case highlight is set, we select the line that contains the requested string
+    let highlightedLine =
+      highlight !== ""
+        ? logMessage.toLowerCase().includes(highlight.toLowerCase())
+        : false;
+
+    // if starts with multiple spaces add padding
+    if (substr.startsWith("   ")) {
+      return (
+        <div
+          key={index}
+          className={`${highlightedLine ? classes.highlight : ""}`}
+        >
+          <span className={classes.tab}>{substr}</span>
+        </div>
+      );
+    } else {
+      // for all remaining set default class
+      return (
+        <div
+          key={index}
+          className={`${highlightedLine ? classes.highlight : ""}`}
+        >
+          <span className={classes.ansidefault}>{substr}</span>
+        </div>
+      );
+    }
+  };
+
+  const renderLines = logLines.map((m, i) => {
+    return renderLog(m, i);
+  });
+
+  useEffect(() => {
+    if (loading) {
+      api
+        .invoke(
+          "GET",
+          `/api/v1/namespaces/${namespace}/tenants/${tenant}/pods/${podName}`
+        )
+        .then((res: string) => {
+          setLogLines(res.split("\n"));
+          setLoading(false);
+        })
+        .catch((err) => {
+          setErrorSnackMessage(err);
+          setLoading(false);
+        });
+    }
+  }, [loading, podName, namespace, tenant, setErrorSnackMessage]);
+
+  return (
+    <React.Fragment>
+      <Grid item xs={12} className={classes.actionsTray}>
+        <TextField
+          placeholder="Highlight Line"
+          className={classes.searchField}
+          id="search-resource"
+          label=""
+          onChange={(val) => {
+            setHighlight(val.target.value);
+          }}
+          InputProps={{
+            disableUnderline: true,
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <br />
+      </Grid>
+      <Grid item xs={12}>
+        <Paper>
+          <div className={classes.logList}>{renderLines}</div>
+        </Paper>
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+const connector = connect(null, {
+  setErrorSnackMessage,
+});
+
+export default withStyles(styles)(connector(PodLogs));
