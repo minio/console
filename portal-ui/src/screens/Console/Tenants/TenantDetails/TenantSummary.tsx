5c40bda74285691e22439b49ae2616bfd7445def
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2021-11-08 16:02:40 -0800

    Disable Manage button if (#1198)
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- portal-ui/src/screens/Console/Tenants/TenantDetails/TenantSummary.tsx
+++ portal-ui/src/screens/Console/Tenants/TenantDetails/TenantSummary.tsx
@@ -34,6 +34,7 @@ import UpdateTenantModal from "./UpdateTenantModal";
 import { AppState } from "../../../../store";
 import history from "./../../../../history";
 import { CircleIcon } from "../../../../icons";
+import { tenantIsOnline } from "../ListTenants/utils";
 
 interface ITenantsSummary {
   classes: any;
@@ -328,6 +329,7 @@ const TenantSummary = ({
                       `/namespaces/${tenantNamespace}/tenants/${tenantName}/hop`
                     );
                   }}
+                  disabled={!tenant || !tenantIsOnline(tenant)}
                 >
                   Manage Tenant
                 </Button>
