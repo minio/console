c2f0889ff8143b63f2e766b9a83e2e554b61aa09
Lenin Alevski <alevsk.8772@gmail.com>
2021-11-09 21:41:00 -0800

    STS session token and console session cookie have same duration (#1202)
    - `CONSOLE_STS_DURATION_IN_SECONDS` env renamed to `CONSOLE_STS_DURATION` to support more time formats
    
    Signed-off-by: Lenin Alevski <alevsk.8772@gmail.com>

--- pkg/auth/token/const.go
+++ pkg/auth/token/const.go
@@ -17,7 +17,8 @@
 package token
 
 const (
-	ConsoleSTSDurationSeconds = "CONSOLE_STS_DURATION_SECONDS"
+	ConsoleSTSDurationSeconds = "CONSOLE_STS_DURATION_SECONDS" // (deprecated), set value in seconds for sts session, ie: 3600
+	ConsoleSTSDuration        = "CONSOLE_STS_DURATION"         // time.Duration format, ie: 3600s, 2h45m, 1h, etc
 	ConsolePBKDFPassphrase    = "CONSOLE_PBKDF_PASSPHRASE"
 	ConsolePBKDFSalt          = "CONSOLE_PBKDF_SALT"
 )
