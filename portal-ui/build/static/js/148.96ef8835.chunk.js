ff329818fbe4065ac82d94fdf5b801bcefd7fb3c
Harshavardhana <harsha@minio.io>
2021-12-26 11:44:53 -0800

    Release v0.13.0

--- portal-ui/build/static/js/148.96ef8835.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpportal-ui"]=this["webpackJsonpportal-ui"]||[]).push([[148],{966:function(e,t,i){"use strict";i.r(t);var n=i(2),o=i(398),c=i(816),a=i(0);t.default=function(e){var t=e.open,i=e.bucketName,r=e.object,s=e.onClosePreview;return Object(a.jsx)(n.Fragment,{children:Object(a.jsx)(o.a,{modalOpen:t,title:"Preview - ".concat(null===r||void 0===r?void 0:r.name),onClose:s,wideLimit:!1,children:Object(a.jsx)(c.a,{bucketName:i,object:r})})})}}}]);
-//# sourceMappingURL=148.96ef8835.chunk.js.map
