a6289f68ae65417a306f03b190303142ff3bdfae
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2022-01-12 10:43:57 -0800

    Detect basename for react router
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- portal-ui/src/history.ts
+++ portal-ui/src/history.ts
@@ -3,4 +3,10 @@ import { BrowserHistoryBuildOptions } from "history/createBrowserHistory";
 
 let browserHistoryOpts: BrowserHistoryBuildOptions = {};
 
+let basename = document.baseURI.replace(window.location.origin, "");
+
+if (basename !== "") {
+  browserHistoryOpts.basename = basename;
+}
+
 export default createBrowserHistory(browserHistoryOpts);
