25b24ba7465a816e39aaeebc7731abbbf1583be8
cniackz <cniackz@cniackzs-MacBook-Air.local>
2022-01-12 19:13:59 -0500

    to fail test when yarn does not build

--- portal-ui/check-warnings.sh
+++ portal-ui/check-warnings.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-if yarn build | grep "Compiled with warnings"; then
+yell() { echo "$0: $*" >&2; }
+
+die() {
+  yell "$*"
+  cat yarn.log
+  exit 111
+}
+
+try() { "$@" &> yarn.log || die "cannot $*"; }
+
+rm yarn.log
+try yarn build
+
+if cat yarn.log | grep "Compiled with warnings"; then
   echo "There are warnings in the code"
   exit 1
 fi
