a8187029c91faedeaee6068c07a8ec9ac3adb052
Kaan Kabalak <kaan@minio.io>
2022-01-12 21:59:08 -0800

    Start frontend app in separate command

--- portal-ui/tests/scripts/permissions.sh
+++ portal-ui/tests/scripts/permissions.sh
@@ -120,7 +120,8 @@ __init__() {
 }
 
 main() {
-  (yarn start &> /dev/null) & (./console server &> /dev/null) & testcafe "chrome:headless" portal-ui/tests/permissions/ --skip-js-errors
+  (yarn start &> /dev/null)
+  (./console server &> /dev/null) & testcafe "chrome:headless" portal-ui/tests/permissions/ --skip-js-errors
   cleanup
 }
 
