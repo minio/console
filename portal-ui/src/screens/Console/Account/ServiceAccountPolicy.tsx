0bcd3a225c19e48b57e79b21abb45f3838e8f7f0
Adam Stafford <adfost@comcast.net>
2022-02-03 09:04:57 -0800

    saving

---
+++ portal-ui/src/screens/Console/Account/ServiceAccountPolicy.tsx
@@ -0,0 +1,164 @@
+// This file is part of MinIO Console Server
+// Copyright (c) 2021 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { Button, SelectChangeEvent } from "@mui/material";
+import { Theme } from "@mui/material/styles";
+import createStyles from "@mui/styles/createStyles";
+import withStyles from "@mui/styles/withStyles";
+import Grid from "@mui/material/Grid";
+import {
+  formFieldStyles,
+  modalStyleUtils,
+  spacingUtils,
+} from "../Common/FormComponents/common/styleLibrary";
+import { setModalErrorSnackMessage } from "../../../actions";
+import { ErrorResponseHandler } from "../../../common/types";
+import api from "../../../common/api";
+import ModalWrapper from "../Common/ModalWrapper/ModalWrapper";
+import SelectWrapper from "../Common/FormComponents/SelectWrapper/SelectWrapper";
+import { ChangeAccessPolicyIcon } from "../../../icons";
+import CodeMirrorWrapper from "../Common/FormComponents/CodeMirrorWrapper/CodeMirrorWrapper";
+
+const styles = (theme: Theme) =>
+  createStyles({
+    codeMirrorContainer: {
+      marginBottom: 20,
+      "& label": {
+        marginBottom: ".5rem",
+      },
+      "& label + div": {
+        display: "none",
+      },
+    },
+    ...formFieldStyles,
+    ...modalStyleUtils,
+    ...spacingUtils,
+  });
+createStyles({
+  ...modalStyleUtils,
+  ...spacingUtils,
+});
+
+interface IServiceAccountPolicyProps {
+  classes: any;
+  open: boolean;
+  bucketName: string;
+  actualPolicy: string;
+  actualDefinition: string;
+  closeModalAndRefresh: () => void;
+  setModalErrorSnackMessage: typeof setModalErrorSnackMessage;
+}
+
+const ServiceAccountPolicy = ({
+  classes,
+  open,
+  bucketName,
+  actualPolicy,
+  actualDefinition,
+  closeModalAndRefresh,
+  setModalErrorSnackMessage,
+}: IServiceAccountPolicyProps) => {
+  const [addLoading, setAddLoading] = useState<boolean>(false);
+  const [accessPolicy, ServiceAccountPolicy] = useState<string>("");
+  const [policyDefinition, setPolicyDefinition] = useState<string>("");
+  const addRecord = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (addLoading) {
+      return;
+    }
+    setAddLoading(true);
+    api
+      .invoke("GET", `/api/v1/buckets/${bucketName}/set-policy`, {
+        access: accessPolicy,
+        definition: policyDefinition,
+      })
+      .then((res) => {
+        setAddLoading(false);
+        closeModalAndRefresh();
+      })
+      .catch((err: ErrorResponseHandler) => {
+        setAddLoading(false);
+        setModalErrorSnackMessage(err);
+      });
+  };
+
+  useEffect(() => {
+    ServiceAccountPolicy(actualPolicy);
+    setPolicyDefinition(
+      actualDefinition
+        ? JSON.stringify(JSON.parse(actualDefinition), null, 4)
+        : ""
+    );
+  }, [
+    ServiceAccountPolicy,
+    actualPolicy,
+    setPolicyDefinition,
+    actualDefinition,
+  ]);
+
+  return (
+    <ModalWrapper
+      title="Service Account Policy"
+      modalOpen={open}
+      onClose={() => {
+        closeModalAndRefresh();
+      }}
+      titleIcon={<ChangeAccessPolicyIcon />}
+    >
+      <Grid container>
+        <Grid item xs={12} className={classes.codeMirrorContainer}>
+          <CodeMirrorWrapper
+            label={`Service Account Policy`}
+            value={policyDefinition}
+            onBeforeChange={(editor, data, value) => {
+              setPolicyDefinition(value);
+            }}
+            editorHeight={"350px"}
+          />
+        </Grid>
+        <Grid item xs={12} className={classes.modalButtonBar}>
+          <Button
+            type="button"
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+              closeModalAndRefresh();
+            }}
+            disabled={addLoading}
+          >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={addLoading}
+          >
+            Set
+          </Button>
+        </Grid>
+      </Grid>
+    </ModalWrapper>
+  );
+};
+
+const connector = connect(null, {
+  setModalErrorSnackMessage,
+});
+
+export default withStyles(styles)(connector(ServiceAccountPolicy));
