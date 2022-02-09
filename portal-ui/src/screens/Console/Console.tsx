ffc780dbebd7b53294f4496d856052990615152d
Lenin Alevski <alevsk.8772@gmail.com>
2022-02-09 17:56:58 -0600

    Add support for matching multiple resources in SecureComponent
    Signed-off-by: Lenin Alevski <alevsk.8772@gmail.com>

--- portal-ui/src/screens/Console/Console.tsx
+++ portal-ui/src/screens/Console/Console.tsx
@@ -219,6 +219,14 @@ const Console = ({
     {
       component: Buckets,
       path: IAM_PAGES.ADD_BUCKETS,
+      customPermissionFnc: () => {
+        const createBucketResources: string[] =
+          Array.from(Object.keys(session.permissions)) || [];
+        return hasPermission(
+          createBucketResources,
+          IAM_PAGES_PERMISSIONS[IAM_PAGES.ADD_BUCKETS]
+        );
+      },
     },
     {
       component: Buckets,
