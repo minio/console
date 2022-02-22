b130c89297f41516198a5b06004a094b5b95ab9e
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2022-02-22 13:53:39 -0500

    Get Operator files for testing (#1613)

--- portal-ui/tests/scripts/operator.sh
+++ portal-ui/tests/scripts/operator.sh
@@ -36,7 +36,7 @@ die() {
 try() { "$@" || die "cannot $*"; }
 
 function setup_kind() {
-	try kind create cluster --config "${SCRIPT_DIR}/kind-config.yaml"
+	try kind create cluster --config "${SCRIPT_DIR}/../../../operator/testing/kind-config.yaml"
 	echo "Kind is ready"
 	try kubectl get nodes
 }
@@ -44,7 +44,7 @@ function setup_kind() {
 function install_operator() {
 
 	echo "Installing Current Operator"
-	try kubectl apply -k "${SCRIPT_DIR}/resources"
+	try kubectl apply -k "${SCRIPT_DIR}/../../../operator/resources"
 
 	echo "key, value for pod selector in kustomize test"
 	key=name
@@ -142,7 +142,7 @@ function install_tenant() {
 	value=storage-lite
 	echo "Installing lite tenant"
 
-	try kubectl apply -k "${SCRIPT_DIR}/examples/kustomization/tenant-lite"
+	try kubectl apply -k "${SCRIPT_DIR}/../../../operator/examples/kustomization/tenant-lite"
 
 	echo "Waiting for the tenant statefulset, this indicates the tenant is being fulfilled"
 	echo $namespace
