fce387eddc5d655c09b27e215835de8740645178
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2022-02-24 13:14:35 -0800

    Fix Menu
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- portal-ui/tests/operator/list-tenants.ts
+++ portal-ui/tests/operator/list-tenants.ts
@@ -15,25 +15,27 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { diagnosticsElement, supportElement } from "../utils/elements-menu";
-import { Selector } from 'testcafe';
-
+import { Selector } from "testcafe";
 
 fixture("For user with default permissions").page("http://localhost:9090");
 
 test("Create Tenant and List Tenants", async (t) => {
+  const osCount = Selector(
+    `#root > div > main > div[class] > div > div > div > div:nth-child(1) > div > div > div`
+  ).count;
 
-	const osCount = Selector(`#root > div > main > div[class] > div > div > div > div:nth-child(1) > div > div > div`).count;
-
-	await t
-		.navigateTo("http://localhost:9090/login")
-		.typeText("#jwt","anyrandompasswordwillwork")
-		.click("button.MuiButton-root")
-		.click(Selector('button[tabindex="0"][type="button"]').withText('Create Tenant'))
-		.typeText("#tenant-name","thufeb1754epm")
-		.typeText("#namespace","default")
-		.wait(2000)
-		.click("button[tabindex=\"0\"]:nth-of-type(2)")
-		.click(Selector('button[tabindex="0"][type="button"]').withText('Done'))
-		.expect(osCount).eql(2);
-
+  await t
+    .navigateTo("http://localhost:9090/login")
+    .typeText("#jwt", "anyrandompasswordwillwork")
+    .click("button.MuiButton-root")
+    .click(
+      Selector('button[tabindex="0"][type="button"]').withText("Create Tenant")
+    )
+    .typeText("#tenant-name", "thufeb1754epm")
+    .typeText("#namespace", "default")
+    .wait(2000)
+    .click('button[tabindex="0"]:nth-of-type(2)')
+    .click(Selector('button[tabindex="0"][type="button"]').withText("Done"))
+    .expect(osCount)
+    .eql(2);
 });
