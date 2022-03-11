16de01a13e72728a6d27ca2a8be45f4c4fd30a6d
Adam Stafford <adfost@comcast.net>
2022-03-10 17:25:47 -0800

    delete all versions test case:

---
+++ portal-ui/tests/permissions/bucketDeleteAllVersions.ts
@@ -0,0 +1,48 @@
+// This file is part of MinIO Console Server
+// Copyright (c) 2022 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import * as roles from "../utils/roles";
+import * as elements from "../utils/elements";
+import * as functions from "../utils/functions";
+import { bucketsElement } from "../utils/elements-menu";
+import { testBucketBrowseButtonFor } from "../utils/functions";
+import { Selector } from "testcafe";
+import * as constants from "../utils/constants";
+
+fixture("For user with Bucket Delete permissions").page("http://localhost:9090");
+
+test
+  .before(async (t) => {
+    // Create a bucket
+    await functions.setUpBucket(t, "bucketdelete3");
+    await functions.setVersioned(t, "bucketdelete3");
+  })("Object can be uploaded to a bucket", async (t) => {
+    const testBucketBrowseButton = testBucketBrowseButtonFor("bucketdelete3");
+    await t
+      .useRole(roles.bucketReadWrite)
+      .navigateTo("http://localhost:9090/buckets")
+      .click(testBucketBrowseButton)
+      // Upload object to bucket
+      .setFilesToUpload(elements.uploadInput, "../uploads/test.txt")
+      .wait(5000)
+      .click("#object-list-wrapper > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-15j76c0 > div > div:nth-child(1) > div > div.ReactVirtualized__Grid.ReactVirtualized__Table__Grid > div > div")
+      .click(elements.deleteButton)
+      .click(elements.switchInput)
+      .click(Selector("button:enabled").withExactText("Delete").nth(1))
+  }).after(async (t) => {
+    // Cleanup created bucket and corresponding uploads
+    await functions.cleanUpBucketAndUploads(t, "bucketdelete3");
+  });
