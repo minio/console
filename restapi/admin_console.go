4f9d4168ab2dc25669e3370813d1141aae33f166
Jill <jill@minio.io>
2022-04-01 15:45:11 -0700

    Trimmed port from node name

--- restapi/admin_console.go
+++ restapi/admin_console.go
@@ -30,9 +30,15 @@ const logTimeFormat string = "15:04:05 MST 01/02/2006"
 
 // startConsoleLog starts log of the servers
 func startConsoleLog(ctx context.Context, conn WSConn, client MinioAdmin, logRequest LogRequest) error {
-
+	var node string
 	// name of node, default = "" (all)
-	node := logRequest.node
+	if logRequest.node == "all" {
+		node = ""
+	} else {
+		node = logRequest.node
+	}
+
+	trimNode := strings.Split(node, ":")
 	// number of log lines
 	lineCount := 100
 	// type of logs "minio"|"application"|"all" default = "all"
@@ -42,16 +48,19 @@ func startConsoleLog(ctx context.Context, conn WSConn, client MinioAdmin, logReq
 	} else {
 		logKind = "all"
 	}
+
 	// Start listening on all Console Log activity.
-	logCh := client.getLogs(ctx, node, lineCount, logKind)
+	logCh := client.getLogs(ctx, trimNode[0], lineCount, logKind)
 
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
 		case logInfo, ok := <-logCh:
+
 			// zero value returned because the channel is closed and empty
 			if !ok {
+
 				return nil
 			}
 			if logInfo.Err != nil {
