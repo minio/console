b567e4855fc22277c6a538ee2617d5dc374894c1
jinapurapu <65002498+jinapurapu@users.noreply.github.com>
2022-04-22 17:49:24 -0700

    Create service account screen (#1886)

--- portal-ui/src/icons/PasswordKeyIcon.tsx
+++ portal-ui/src/icons/PasswordKeyIcon.tsx
@@ -30,7 +30,7 @@ const PasswordKeyIcon = (props: SVGProps<SVGSVGElement>) => {
         data-name="Trazado 7179"
         d="M141.421,148.182a4.5,4.5,0,0,0-4.3,5.805l-5.188,5.195v3h3l5.187-5.2a4.5,4.5,0,0,0,5.8-3.936,4.39,4.39,0,0,0-.823-3A4.492,4.492,0,0,0,141.421,148.182Zm.5,5a1,1,0,1,1,1-1A1,1,0,0,1,141.92,153.182Z"
         transform="translate(-131.934 -148.182)"
-        fill="#5e5e5e"
+        //fill="#5e5e5e"
       />
       <rect
         id="Rectángulo_1090"
