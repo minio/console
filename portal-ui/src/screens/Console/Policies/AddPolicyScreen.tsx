4cb3f479793ffe7593a9b0717c128cd7fc66e4d5
jinapurapu <jill@minio.io>
2022-05-02 10:57:05 -0700

    Added Error message if space used in policy name

--- portal-ui/src/screens/Console/Policies/AddPolicyScreen.tsx
+++ portal-ui/src/screens/Console/Policies/AddPolicyScreen.tsx
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Theme } from "@mui/material/styles";
 import createStyles from "@mui/styles/createStyles";
 import withStyles from "@mui/styles/withStyles";
@@ -70,6 +70,15 @@ const AddPolicyScreen = ({
   const [addLoading, setAddLoading] = useState<boolean>(false);
   const [policyName, setPolicyName] = useState<string>("");
   const [policyDefinition, setPolicyDefinition] = useState<string>("");
+
+  useEffect(() => {
+    if (policyName.indexOf(' ') !==-1) {
+      setErrorSnackMessage({
+        errorMessage: "Policy name cannot contain spaces",
+        detailedError: "",
+      });
+    }
+  }, [policyName]);
 
   const addRecord = (event: React.FormEvent) => {
 
