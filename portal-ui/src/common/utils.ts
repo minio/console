175b87c3558f275591b0e2829f35cd4172440a7b
Harshavardhana <harsha@minio.io>
2022-05-23 00:51:59 -0700

    fix: getTimeFromTimestamp() incorrect padding for days (#2029)
    use a simpler function to return localeString() instead.

--- portal-ui/src/common/utils.ts
+++ portal-ui/src/common/utils.ts
@@ -557,22 +557,13 @@ export const getTimeFromTimestamp = (
   fullDate: boolean = false
 ) => {
   const timestampToInt = parseInt(timestamp);
-
   if (isNaN(timestampToInt)) {
     return "";
   }
   const dateObject = new Date(timestampToInt * 1000);
 
   if (fullDate) {
-    return `${dateObject.getFullYear()}-${String(
-      dateObject.getMonth() + 1
-    ).padStart(2, "0")}-${String(dateObject.getDay()).padStart(
-      2,
-      "0"
-    )} ${dateObject.getHours()}:${String(dateObject.getMinutes()).padStart(
-      2,
-      "0"
-    )}:${String(dateObject.getSeconds()).padStart(2, "0")}`;
+    return dateObject.toLocaleString();
   }
   return `${dateObject.getHours()}:${String(dateObject.getMinutes()).padStart(
     2,
