284dffaa5d5a349f4e7bc4bece93b453c4a7b854
Javier <reivaj0705@gmail.com>
2022-07-22 17:18:51 -0500

    Auto register API key once it is retrieved from subnet
    Remove unused imports
    
    Fix useEffect
    
    Remove console
    
    Change the way classes are loaded into component

--- portal-ui/src/screens/Console/Support/utils.tsx
+++ portal-ui/src/screens/Console/Support/utils.tsx
@@ -1,5 +1,8 @@
 import { Box, Grid, Link } from "@mui/material";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import { CopyIcon, SettingsIcon } from "../../../icons";
+import FormSwitchWrapper from "../Common/FormComponents/FormSwitchWrapper/FormSwitchWrapper";
+import InputBoxWrapper from "../Common/FormComponents/InputBoxWrapper/InputBoxWrapper";
 import RegistrationStatusBanner from "./RegistrationStatusBanner";
 
 export const FormTitle = ({
@@ -54,6 +57,101 @@ export const ClusterRegistered = ({
           to avail support for this MinIO cluster
         </Box>
       </Grid>
+    </Fragment>
+  );
+};
+
+export const ProxyConfiguration = ({ linkClass }: { linkClass: string }) => {
+  const proxyConfigurationCommand =
+    "mc admin config set {alias} subnet proxy={proxy}";
+  const [displaySubnetProxy, setDisplaySubnetProxy] = useState(false);
+  return (
+    <Fragment>
+      <Box
+        sx={{
+          border: "1px solid #eaeaea",
+          borderRadius: "2px",
+          display: "flex",
+          padding: "23px",
+          marginTop: "40px",
+          alignItems: "start",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexFlow: "column",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              "& .min-icon": {
+                height: "22px",
+                width: "22px",
+              },
+            }}
+          >
+            <SettingsIcon />
+            <div style={{ marginLeft: "10px", fontWeight: 600 }}>
+              Proxy Configuration
+            </div>
+          </Box>
+          <Box
+            sx={{
+              marginTop: "10px",
+              marginBottom: "10px",
+              fontSize: "14px",
+            }}
+          >
+            For airgap/firewalled environments it is possible to{" "}
+            <Link
+              className={linkClass}
+              href="https://docs.min.io/docs/minio-server-configuration-guide.html?ref=con"
+              target="_blank"
+            >
+              configure a proxy
+            </Link>{" "}
+            to connect to SUBNET .
+          </Box>
+          <Box>
+            {displaySubnetProxy && (
+              <InputBoxWrapper
+                disabled
+                id="subnetProxy"
+                name="subnetProxy"
+                placeholder=""
+                onChange={() => {}}
+                label=""
+                value={proxyConfigurationCommand}
+                overlayIcon={<CopyIcon />}
+                extraInputProps={{
+                  readOnly: true,
+                }}
+                overlayAction={() =>
+                  navigator.clipboard.writeText(proxyConfigurationCommand)
+                }
+              />
+            )}
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+          }}
+        >
+          <FormSwitchWrapper
+            value="enableProxy"
+            id="enableProxy"
+            name="enableProxy"
+            checked={displaySubnetProxy}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setDisplaySubnetProxy(event.target.checked);
+            }}
+          />
+        </Box>
+      </Box>
     </Fragment>
   );
 };
