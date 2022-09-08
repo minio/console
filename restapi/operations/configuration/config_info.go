94fdba59908ad067f620ad9430b99bd1d5c9bd2e
Javier Adriel <reivaj0705@gmail.com>
2022-09-08 11:13:22 -0500

    Run lint in missing files (#2300)

--- restapi/operations/configuration/config_info.go
+++ restapi/operations/configuration/config_info.go
@@ -63,7 +63,7 @@ func (o *ConfigInfo) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if rCtx != nil {
 		*r = *rCtx
 	}
-	Params := NewConfigInfoParams()
+	var Params = NewConfigInfoParams()
 	uprinc, aCtx, err := o.Context.Authorize(r, route)
 	if err != nil {
 		o.Context.Respond(rw, r, route.Produces, route, err)
@@ -84,4 +84,5 @@ func (o *ConfigInfo) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 	res := o.Handler.Handle(Params, principal) // actually handle the request
 	o.Context.Respond(rw, r, route.Produces, route, res)
+
 }
