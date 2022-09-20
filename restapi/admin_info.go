7d2d2e493534d955dc1012393db7c98358a046a0
Anis Elleuch <anis@min.io>
2022-09-20 13:24:38 +0100

    Skip TLS verification for requests to local address
    If CONSOLE_MINIO_URL is pointing to 127.0.0.1, skip the TLS verificatin

--- restapi/admin_info.go
+++ restapi/admin_info.go
@@ -946,7 +946,7 @@ func getUsageWidgetsForDeployment(ctx context.Context, prometheusURL string, mAd
 }
 
 func unmarshalPrometheus(ctx context.Context, endpoint string, data interface{}) bool {
-	httpClnt := GetConsoleHTTPClient()
+	httpClnt := GetConsoleHTTPClient(endpoint)
 	resp, err := httpClnt.Get(endpoint)
 	if err != nil {
 		ErrorWithContext(ctx, fmt.Errorf("Unable to fetch labels from prometheus (%s)", resp.Status))
@@ -973,7 +973,7 @@ func testPrometheusURL(ctx context.Context, url string) bool {
 		ErrorWithContext(ctx, fmt.Errorf("error Building Request: (%v)", err))
 		return false
 	}
-	response, err := GetConsoleHTTPClient().Do(req)
+	response, err := GetConsoleHTTPClient(url).Do(req)
 	if err != nil {
 		ErrorWithContext(ctx, fmt.Errorf("default Prometheus URL not reachable, trying root testing: (%v)", err))
 		newTestURL := req.URL.Scheme + "://" + req.URL.Host + "/-/healthy"
@@ -982,7 +982,7 @@ func testPrometheusURL(ctx context.Context, url string) bool {
 			ErrorWithContext(ctx, fmt.Errorf("error Building Root Request: (%v)", err))
 			return false
 		}
-		rootResponse, err := GetConsoleHTTPClient().Do(req2)
+		rootResponse, err := GetConsoleHTTPClient(newTestURL).Do(req2)
 		if err != nil {
 			// URL & Root tests didn't work. Prometheus not reachable
 			ErrorWithContext(ctx, fmt.Errorf("root Prometheus URL not reachable: (%v)", err))
