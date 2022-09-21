5c579a9223d4be190322275acbb797aa8e0dce5b
Lenin Alevski <alevsk.8772@gmail.com>
2022-09-21 10:42:42 -0700

    Add support for edit/add/remove environment variables to MinIO tenant
    Signed-off-by: Lenin Alevski <alevsk.8772@gmail.com>

--- operatorapi/tenants.go
+++ operatorapi/tenants.go
@@ -110,6 +110,24 @@ func registerTenantHandlers(api *operations.OperatorAPI) {
 		return operator_api.NewTenantDetailsOK().WithPayload(resp)
 	})
 
+	// Tenant Configuration details
+	// Tenant Security details
+	api.OperatorAPITenantConfigurationHandler = operator_api.TenantConfigurationHandlerFunc(func(params operator_api.TenantConfigurationParams, session *models.Principal) middleware.Responder {
+		resp, err := getTenantConfigurationResponse(session, params)
+		if err != nil {
+			return operator_api.NewTenantConfigurationDefault(int(err.Code)).WithPayload(err)
+		}
+		return operator_api.NewTenantConfigurationOK().WithPayload(resp)
+	})
+	// Update Tenant Configuration
+	api.OperatorAPIUpdateTenantConfigurationHandler = operator_api.UpdateTenantConfigurationHandlerFunc(func(params operator_api.UpdateTenantConfigurationParams, session *models.Principal) middleware.Responder {
+		err := getUpdateTenantConfigurationResponse(session, params)
+		if err != nil {
+			return operator_api.NewUpdateTenantConfigurationDefault(int(err.Code)).WithPayload(err)
+		}
+		return operator_api.NewUpdateTenantConfigurationNoContent()
+	})
+
 	// Tenant Security details
 	api.OperatorAPITenantSecurityHandler = operator_api.TenantSecurityHandlerFunc(func(params operator_api.TenantSecurityParams, session *models.Principal) middleware.Responder {
 		resp, err := getTenantSecurityResponse(session, params)
@@ -968,6 +986,123 @@ func getSetTenantAdministratorsResponse(session *models.Principal, params operat
 		if err := restapi.SetPolicy(ctx, adminClient, "consoleAdmin", group, "group"); err != nil {
 			return restapi.ErrorWithContext(ctx, err)
 		}
+	}
+	return nil
+}
+
+func getTenantConfigurationResponse(session *models.Principal, params operator_api.TenantConfigurationParams) (*models.TenantConfigurationResponse, *models.Error) {
+	ctx, cancel := context.WithCancel(params.HTTPRequest.Context())
+	defer cancel()
+	opClientClientSet, err := cluster.OperatorClient(session.STSSessionToken)
+	if err != nil {
+		return nil, restapi.ErrorWithContext(ctx, err)
+	}
+	opClient := &operatorClient{
+		client: opClientClientSet,
+	}
+	minTenant, err := getTenant(ctx, opClient, params.Namespace, params.Tenant)
+	if err != nil {
+		return nil, restapi.ErrorWithContext(ctx, err)
+	}
+	// get Kubernetes Client
+	clientSet, err := cluster.K8sClient(session.STSSessionToken)
+	k8sClient := k8sClient{
+		client: clientSet,
+	}
+	if err != nil {
+		return nil, restapi.ErrorWithContext(ctx, err)
+	}
+	tenantConfiguration, err := GetTenantConfiguration(ctx, &k8sClient, minTenant)
+	if err != nil {
+		return nil, restapi.ErrorWithContext(ctx, err)
+	}
+	delete(tenantConfiguration, "accesskey")
+	delete(tenantConfiguration, "secretkey")
+	var envVars []*models.EnvironmentVariable
+	for key, value := range tenantConfiguration {
+		envVars = append(envVars, &models.EnvironmentVariable{
+			Key:   key,
+			Value: value,
+		})
+	}
+	sort.Slice(envVars, func(i, j int) bool {
+		return envVars[i].Key < envVars[j].Key
+	})
+	configurationInfo := &models.TenantConfigurationResponse{EnvironmentVariables: envVars}
+	return configurationInfo, nil
+}
+
+func getUpdateTenantConfigurationResponse(session *models.Principal, params operator_api.UpdateTenantConfigurationParams) *models.Error {
+	ctx, cancel := context.WithCancel(params.HTTPRequest.Context())
+	defer cancel()
+	opClientClientSet, err := cluster.OperatorClient(session.STSSessionToken)
+	if err != nil {
+		return restapi.ErrorWithContext(ctx, err)
+	}
+	// get Kubernetes Client
+	clientSet, err := cluster.K8sClient(session.STSSessionToken)
+	if err != nil {
+		return restapi.ErrorWithContext(ctx, err)
+	}
+	k8sClient := k8sClient{
+		client: clientSet,
+	}
+	opClient := &operatorClient{
+		client: opClientClientSet,
+	}
+	if err := updateTenantConfigurationFile(ctx, opClient, &k8sClient, params.Namespace, params); err != nil {
+		return restapi.ErrorWithContext(ctx, err, errors.New("unable to update tenant configuration"))
+	}
+	return nil
+}
+
+func updateTenantConfigurationFile(ctx context.Context, operatorClient OperatorClientI, client K8sClientI, namespace string, params operator_api.UpdateTenantConfigurationParams) error {
+	tenant, err := operatorClient.TenantGet(ctx, namespace, params.Tenant, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	tenantConfiguration, err := GetTenantConfiguration(ctx, client, tenant)
+	if err != nil {
+		return err
+	}
+
+	delete(tenantConfiguration, "accesskey")
+	delete(tenantConfiguration, "secretkey")
+
+	requestBody := params.Body
+	if requestBody == nil {
+		return errors.New("missing request body")
+	}
+	// Patch tenant configuration file with the new values provided by the user
+	for _, envVar := range requestBody.EnvironmentVariables {
+		if envVar.Key == "" {
+			continue
+		}
+		tenantConfiguration[envVar.Key] = envVar.Value
+	}
+	// Remove existing values from configuration file
+	for _, keyToBeDeleted := range requestBody.KeysToBeDeleted {
+		delete(tenantConfiguration, keyToBeDeleted)
+	}
+
+	if !tenant.HasConfigurationSecret() {
+		return errors.New("tenant configuration file not found")
+	}
+	tenantConfigurationSecret, err := client.getSecret(ctx, tenant.Namespace, tenant.Spec.Configuration.Name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	tenantConfigurationSecret.Data["config.env"] = []byte(GenerateTenantConfigurationFile(tenantConfiguration))
+	_, err = client.updateSecret(ctx, namespace, tenantConfigurationSecret, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+	// Restart all MinIO pods at the same time for they to take the new configuration
+	err = client.deletePodCollection(ctx, namespace, metav1.DeleteOptions{}, metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("%s=%s", miniov2.TenantLabel, tenant.Name),
+	})
+	if err != nil {
+		return err
 	}
 	return nil
 }
