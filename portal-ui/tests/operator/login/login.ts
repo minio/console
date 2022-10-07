43db7729c4145320baed1f59a45e0b0165bbd649
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2022-10-07 16:42:44 -0400

    Splitting Operator UI Tests for faster execution (#2366)

--- portal-ui/tests/operator/login/login.ts
+++ portal-ui/tests/operator/login/login.ts
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import * as roles from "../utils/roles";
-import * as elements from "../utils/elements";
-import { diagnosticsElement, supportElement } from "../utils/elements-menu";
+import * as roles from "../../utils/roles";
+import * as elements from "../../utils/elements";
+import { diagnosticsElement, supportElement } from "../../utils/elements-menu";
 
 fixture("For user with default permissions").page("http://localhost:9090");
 
