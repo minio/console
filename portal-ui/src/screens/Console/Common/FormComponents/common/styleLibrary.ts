287af260b7db6ad360a2d99e65fc99cf7723b0f7
Kaan Kabalak <kaan@minio.io>
2023-01-05 12:27:36 -0800

    Fix Copy URL snackbar styling in Share File modal (#2549)

--- portal-ui/src/screens/Console/Common/FormComponents/common/styleLibrary.ts
+++ portal-ui/src/screens/Console/Common/FormComponents/common/styleLibrary.ts
@@ -750,7 +750,6 @@ export const snackBarCommon = {
     fontWeight: 400,
     fontFamily: "Lato, sans-serif",
     fontSize: 14,
-    padding: "0px 20px 0px 20px;",
     boxShadow: "none" as const,
     "&.MuiPaper-root.MuiSnackbarContent-root": {
       borderRadius: "0px 0px 5px 5px",
@@ -761,6 +760,9 @@ export const snackBarCommon = {
       width: "100%",
       overflowX: "hidden",
       textOverflow: "ellipsis",
+    },
+    "&.MuiPaper-root": {
+      padding: "0px 20px 0px 20px",
     },
   },
   errorSnackBar: {
