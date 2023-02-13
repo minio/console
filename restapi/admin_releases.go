86f8b5881eebba368be8b4727995d18f6a0ad351
Javier <reivaj0705@gmail.com>
2023-02-13 16:43:05 -0600

    Change updates service url

--- restapi/admin_releases.go
+++ restapi/admin_releases.go
@@ -32,7 +32,7 @@ import (
 
 var (
 	releaseServiceHostEnvVar  = "RELEASE_SERVICE_HOST"
-	defaultReleaseServiceHost = "https://release-service.apps.min.dev"
+	defaultReleaseServiceHost = "https://enterprise-updates.ic.min.dev"
 )
 
 func registerReleasesHandlers(api *operations.ConsoleAPI) {
