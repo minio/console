68b474851b0d036dce774bace3973e7731ba1e63
cesnietor <>
2023-04-19 13:00:21 -0700

    Show progress bar when loading Usage Info
    On Metrics screen there was no visual feedback whenever
    the Sync button was pressed nor it was being disabled

--- portal-ui/src/screens/Console/Dashboard/Prometheus/PrDashboard.tsx
+++ portal-ui/src/screens/Console/Dashboard/Prometheus/PrDashboard.tsx
@@ -63,6 +63,7 @@ const styles = (theme: Theme) =>
 
 const PrDashboard = ({ apiPrefix = "admin", usage }: IPrDashboard) => {
   const dispatch = useAppDispatch();
+  const loadingUsage = useSelector((state: AppState) => state.dashboard.loadingUsage);
   const zoomOpen = useSelector(
     (state: AppState) => state.dashboard.zoom.openZoom
   );
@@ -237,6 +238,7 @@ const PrDashboard = ({ apiPrefix = "admin", usage }: IPrDashboard) => {
                       onClick={() => {
                         dispatch(getUsageAsync());
                       }}
+                      disabled={loadingUsage}
                       icon={<SyncIcon />}
                       label={"Sync"}
                     />
@@ -328,8 +330,8 @@ const PrDashboard = ({ apiPrefix = "admin", usage }: IPrDashboard) => {
           index={usage?.advancedMetricsStatus === "not configured" ? 0 : 3}
           value={curTab}
         >
-          {!usage && <LinearProgress />}
-          {usage && <BasicDashboard usage={usage} />}
+          {(!usage || loadingUsage) && <LinearProgress />}
+          {usage && !loadingUsage && <BasicDashboard usage={usage} />}
         </TabPanel>
       </Grid>
     </PageLayout>
