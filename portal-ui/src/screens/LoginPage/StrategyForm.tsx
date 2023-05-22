dc90db6591d0eff101127cf9c7f5144c20cfdbdc
Alex <33497058+bexsoft@users.noreply.github.com>
2023-05-22 11:20:45 -0600

    Changed SSO Login screen to hide login form by default (#2807)
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

--- portal-ui/src/screens/LoginPage/StrategyForm.tsx
+++ portal-ui/src/screens/LoginPage/StrategyForm.tsx
@@ -14,72 +14,47 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import Grid from "@mui/material/Grid";
-import React from "react";
+import React, { Fragment, useState } from "react";
 import {
+  Box,
   Button,
+  DropdownSelector,
+  Grid,
+  InputBox,
   LockFilledIcon,
   LogoutIcon,
   PasswordKeyIcon,
+  Select,
   UserFilledIcon,
 } from "mds";
-import { setAccessKey, setSecretKey, setSTS, setUseSTS } from "./loginSlice";
 import {
-  InputAdornment,
-  LinearProgress,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-} from "@mui/material";
+  setAccessKey,
+  setDisplayEmbeddedIDPForms,
+  setSecretKey,
+  setSTS,
+  setUseSTS,
+} from "./loginSlice";
+import { LinearProgress } from "@mui/material";
 import { AppState, useAppDispatch } from "../../store";
 import { useSelector } from "react-redux";
-import { LoginField } from "./LoginField";
-import makeStyles from "@mui/styles/makeStyles";
-import { Theme } from "@mui/material/styles";
-import createStyles from "@mui/styles/createStyles";
-import { spacingUtils } from "../Console/Common/FormComponents/common/styleLibrary";
 import { doLoginAsync } from "./loginThunks";
 import { IStrategyForm } from "./types";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      overflow: "auto",
-    },
-    form: {
-      width: "100%", // Fix IE 11 issue.
-    },
-    submit: {
-      margin: "30px 0px 8px",
-      height: 40,
-      width: "100%",
-      boxShadow: "none",
-      padding: "16px 30px",
-    },
-    submitContainer: {
-      textAlign: "right",
-      marginTop: 30,
-    },
-    linearPredef: {
-      height: 10,
-    },
-    ...spacingUtils,
-  })
-);
-
 const StrategyForm = ({ redirectRules }: IStrategyForm) => {
   const dispatch = useAppDispatch();
-  const classes = useStyles();
+
+  const [ssoOptionsOpen, ssoOptionsSetOpen] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = React.useState<
+    (EventTarget & HTMLButtonElement) | null
+  >(null);
 
   const accessKey = useSelector((state: AppState) => state.login.accessKey);
   const secretKey = useSelector((state: AppState) => state.login.secretKey);
   const sts = useSelector((state: AppState) => state.login.sts);
   const useSTS = useSelector((state: AppState) => state.login.useSTS);
+  const displaySSOForm = useSelector(
+    (state: AppState) => state.login.ssoEmbeddedIDPDisplay
+  );
 
   const loginSending = useSelector(
     (state: AppState) => state.login.loginSending
@@ -90,177 +65,207 @@ const StrategyForm = ({ redirectRules }: IStrategyForm) => {
     dispatch(doLoginAsync());
   };
 
-  let ssoOptions: React.ReactNode = null;
+  let selectOptions = [
+    {
+      label: useSTS ? "Use Credentials" : "Use STS",
+      value: useSTS ? "use-sts-cred" : "use-sts",
+    },
+  ];
+  let ssoOptions: any[] = [];
 
   if (redirectRules.length > 0) {
-    ssoOptions = redirectRules.map((r, idx) => (
-      <MenuItem
-        value={r.redirect}
-        key={`sso-login-option-${idx}`}
-        className={classes.ssoMenuItem}
-        divider={true}
-      >
-        <LogoutIcon
-          className={classes.ssoLoginIcon}
-          style={{ width: 16, height: 16, marginRight: 8 }}
-        />
-        {r.displayName}
-        {r.serviceType ? ` - ${r.serviceType}` : ""}
-      </MenuItem>
-    ));
+    ssoOptions = redirectRules.map((r) => ({
+      label: `${r.displayName}${r.serviceType ? ` - ${r.serviceType}` : ""}`,
+      value: r.redirect,
+      icon: <LogoutIcon />,
+    }));
+
+    selectOptions = [
+      { label: "Use Credentials", value: "use-sts-cred" },
+      { label: "Use STS", value: "use-sts" },
+    ];
   }
 
-  const extraActionSelector = (e: SelectChangeEvent) => {
-    const value = e.target.value;
-
+  const extraActionSelector = (value: string) => {
     if (value) {
-      console.log(value);
-      if (value.includes("use-sts")) {
-        console.log("si");
-        dispatch(setUseSTS(!useSTS));
+      if (redirectRules.length > 0) {
+        let stsState = true;
+
+        if (value === "use-sts-cred") {
+          stsState = false;
+        }
+
+        dispatch(setUseSTS(stsState));
+        dispatch(setDisplayEmbeddedIDPForms(true));
 
         return;
       }
 
-      window.location.href = e.target.value as string;
+      if (value.includes("use-sts")) {
+        dispatch(setUseSTS(!useSTS));
+        return;
+      }
     }
+  };
+
+  const submitSSOInitRequest = (value: string) => {
+    window.location.href = value;
   };
 
   return (
     <React.Fragment>
-      <form className={classes.form} noValidate onSubmit={formSubmit}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} className={classes.spacerBottom}>
-            <LoginField
-              fullWidth
-              id="accessKey"
-              className={classes.inputField}
-              value={accessKey}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                dispatch(setAccessKey(e.target.value))
+      {redirectRules.length > 0 && (
+        <Fragment>
+          <Box sx={{ marginBottom: 40 }}>
+            <Button
+              id={"SSOSelector"}
+              variant={"subAction"}
+              label={
+                redirectRules.length === 1
+                  ? `${redirectRules[0].displayName}${
+                      redirectRules[0].serviceType
+                        ? ` - ${redirectRules[0].serviceType}`
+                        : ""
+                    }`
+                  : `Login with SSO`
               }
-              placeholder={useSTS ? "STS Username" : "Username"}
-              name="accessKey"
-              autoComplete="username"
-              disabled={loginSending}
-              variant={"outlined"}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment
-                    position="start"
-                    className={classes.iconColor}
-                  >
-                    <UserFilledIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} className={useSTS ? classes.spacerBottom : ""}>
-            <LoginField
               fullWidth
-              className={classes.inputField}
-              value={secretKey}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                dispatch(setSecretKey(e.target.value))
-              }
-              name="secretKey"
-              type="password"
-              id="secretKey"
-              autoComplete="current-password"
-              disabled={loginSending}
-              placeholder={useSTS ? "STS Secret" : "Password"}
-              variant={"outlined"}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment
-                    position="start"
-                    className={classes.iconColor}
-                  >
-                    <LockFilledIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          {useSTS && (
-            <Grid item xs={12} className={classes.spacerBottom}>
-              <LoginField
-                fullWidth
-                id="sts"
-                className={classes.inputField}
-                value={sts}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  dispatch(setSTS(e.target.value))
+              sx={{ height: 50 }}
+              onClick={(e) => {
+                if (redirectRules.length > 1) {
+                  ssoOptionsSetOpen(!ssoOptionsOpen);
+                  setAnchorEl(e.currentTarget);
+                  return;
                 }
-                placeholder={"STS Token"}
-                name="STS"
-                autoComplete="sts"
-                disabled={loginSending}
-                variant={"outlined"}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment
-                      position="start"
-                      className={classes.iconColor}
-                    >
-                      <PasswordKeyIcon />
-                    </InputAdornment>
-                  ),
+                submitSSOInitRequest(redirectRules[0].redirect);
+              }}
+            />
+            {redirectRules.length > 1 && (
+              <DropdownSelector
+                options={ssoOptions}
+                selectedOption={""}
+                onSelect={(nValue) => submitSSOInitRequest(nValue)}
+                hideTriggerAction={() => {
+                  ssoOptionsSetOpen(false);
                 }}
+                open={ssoOptionsOpen}
+                anchorEl={anchorEl}
+              />
+            )}
+          </Box>
+        </Fragment>
+      )}
+
+      <form noValidate onSubmit={formSubmit} style={{ width: "100%" }}>
+        {((displaySSOForm && redirectRules.length > 0) ||
+          redirectRules.length === 0) && (
+          <Fragment>
+            <Grid
+              container
+              sx={{
+                marginTop: redirectRules.length > 0 ? 55 : 0,
+              }}
+            >
+              <Grid item xs={12} sx={{ marginBottom: 14 }}>
+                <InputBox
+                  fullWidth
+                  id="accessKey"
+                  value={accessKey}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    dispatch(setAccessKey(e.target.value))
+                  }
+                  placeholder={useSTS ? "STS Username" : "Username"}
+                  name="accessKey"
+                  autoComplete="username"
+                  disabled={loginSending}
+                  startIcon={<UserFilledIcon />}
+                />
+              </Grid>
+              <Grid item xs={12} sx={{ marginBottom: useSTS ? 14 : 0 }}>
+                <InputBox
+                  fullWidth
+                  value={secretKey}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    dispatch(setSecretKey(e.target.value))
+                  }
+                  name="secretKey"
+                  type="password"
+                  id="secretKey"
+                  autoComplete="current-password"
+                  disabled={loginSending}
+                  placeholder={useSTS ? "STS Secret" : "Password"}
+                  startIcon={<LockFilledIcon />}
+                />
+              </Grid>
+              {useSTS && (
+                <Grid item xs={12}>
+                  <InputBox
+                    fullWidth
+                    id="sts"
+                    value={sts}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      dispatch(setSTS(e.target.value))
+                    }
+                    placeholder={"STS Token"}
+                    name="STS"
+                    autoComplete="sts"
+                    disabled={loginSending}
+                    startIcon={<PasswordKeyIcon />}
+                  />
+                </Grid>
+              )}
+            </Grid>
+
+            <Grid
+              item
+              xs={12}
+              sx={{
+                textAlign: "right",
+                marginTop: 30,
+              }}
+            >
+              <Button
+                type="submit"
+                variant="callAction"
+                color="primary"
+                id="do-login"
+                disabled={
+                  (!useSTS && (accessKey === "" || secretKey === "")) ||
+                  (useSTS &&
+                    (accessKey === "" || secretKey === "" || sts === "")) ||
+                  loginSending
+                }
+                label={"Login"}
+                sx={{
+                  margin: "30px 0px 8px",
+                  height: 40,
+                  width: "100%",
+                  boxShadow: "none",
+                  padding: "16px 30px",
+                }}
+                fullWidth
               />
             </Grid>
-          )}
-        </Grid>
-
-        <Grid item xs={12} className={classes.submitContainer}>
-          <Button
-            type="submit"
-            variant="callAction"
-            color="primary"
-            id="do-login"
-            className={classes.submit}
-            disabled={
-              (!useSTS && (accessKey === "" || secretKey === "")) ||
-              (useSTS && sts === "") ||
-              loginSending
-            }
-            label={"Login"}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} className={classes.linearPredef}>
-          {loginSending && <LinearProgress />}
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          className={classes.linearPredef}
-          sx={{ marginTop: "16px" }}
-        >
+            <Grid
+              item
+              xs={12}
+              sx={{
+                height: 10,
+              }}
+            >
+              {loginSending && <LinearProgress />}
+            </Grid>
+          </Fragment>
+        )}
+        <Grid item xs={12} sx={{ marginTop: 45 }}>
           <Select
             id="alternativeMethods"
             name="alternativeMethods"
+            fixedLabel="Other Authentication Methods"
+            options={selectOptions}
             onChange={extraActionSelector}
-            displayEmpty
-            className={classes.ssoSelect}
-            renderValue={() => "Other Authentication Methods"}
-            sx={{
-              width: "100%",
-              height: "38px",
-              fontSize: "14px",
-              borderRadius: "4px",
-            }}
-          >
-            <MenuItem
-              value={useSTS ? "use-sts-cred" : "use-sts"}
-              className={classes.ssoMenuItem}
-              divider={redirectRules.length > 0}
-            >
-              {useSTS ? "Use Credentials" : "Use STS"}
-            </MenuItem>
-            {ssoOptions}
-          </Select>
+            value={""}
+          />
         </Grid>
       </form>
     </React.Fragment>
--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -7029,9 +7029,6 @@ func init() {
         "animatedLogin": {
           "type": "boolean"
         },
-        "isDirectPV": {
-          "type": "boolean"
-        },
         "isK8S": {
           "type": "boolean"
         },
@@ -16161,9 +16158,6 @@ func init() {
       "type": "object",
       "properties": {
         "animatedLogin": {
-          "type": "boolean"
-        },
-        "isDirectPV": {
           "type": "boolean"
         },
         "isK8S": {
