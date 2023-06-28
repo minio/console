d1ae271111fc4ec8c286b0bb1a9602d9a7ee5976
Alex <33497058+bexsoft@users.noreply.github.com>
2023-06-28 13:05:18 -0600

    Replaced CodeMirrorWrapper internal components (#2910)
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

--- portal-ui/src/screens/Console/Common/FormComponents/CodeMirrorWrapper/CodeMirrorWrapper.tsx
+++ portal-ui/src/screens/Console/Common/FormComponents/CodeMirrorWrapper/CodeMirrorWrapper.tsx
@@ -14,16 +14,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import React from "react";
-import Grid from "@mui/material/Grid";
-import { Box, InputLabel, Tooltip } from "@mui/material";
-import { Theme } from "@mui/material/styles";
-import createStyles from "@mui/styles/createStyles";
-import withStyles from "@mui/styles/withStyles";
-import { Button, CopyIcon, HelpIcon } from "mds";
-import { fieldBasic } from "../common/styleLibrary";
+import React, { Fragment } from "react";
+import { Button, CodeEditor, CopyIcon } from "mds";
 import CopyToClipboard from "react-copy-to-clipboard";
-import CodeEditor from "@uiw/react-textarea-code-editor";
 import TooltipWrapper from "../../TooltipWrapper/TooltipWrapper";
 
 interface ICodeWrapper {
@@ -31,98 +24,28 @@ interface ICodeWrapper {
   label?: string;
   mode?: string;
   tooltip?: string;
-  classes: any;
-  onChange?: (editor: any, data: any, value: string) => any;
-  onBeforeChange: (editor: any, data: any, value: string) => any;
-  readOnly?: boolean;
-  editorHeight?: string;
+  onChange: (value: string) => any;
+  editorHeight?: string | number;
 }
-
-const styles = (theme: Theme) =>
-  createStyles({
-    ...fieldBasic,
-  });
 
 const CodeMirrorWrapper = ({
   value,
   label = "",
   tooltip = "",
   mode = "json",
-  classes,
-  onBeforeChange,
-  readOnly = false,
-  editorHeight = "250px",
+  onChange,
+  editorHeight = 250,
 }: ICodeWrapper) => {
   return (
-    <React.Fragment>
-      <Grid item xs={12} sx={{ marginBottom: "10px" }}>
-        <InputLabel className={classes.inputLabel}>
-          <span>{label}</span>
-          {tooltip !== "" && (
-            <div className={classes.tooltipContainer}>
-              <Tooltip title={tooltip} placement="top-start">
-                <div className={classes.tooltip}>
-                  <HelpIcon />
-                </div>
-              </Tooltip>
-            </div>
-          )}
-        </InputLabel>
-      </Grid>
-
-      <Grid
-        item
-        xs={12}
-        style={{
-          maxHeight: editorHeight,
-          overflow: "auto",
-          border: "1px solid #eaeaea",
-        }}
-      >
-        <CodeEditor
-          value={value}
-          language={mode}
-          onChange={(evn) => {
-            onBeforeChange(null, null, evn.target.value);
-          }}
-          id={"code_wrapper"}
-          padding={15}
-          style={{
-            fontSize: 12,
-            backgroundColor: "#fefefe",
-            fontFamily:
-              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-            minHeight: editorHeight || "initial",
-            color: "#000000",
-          }}
-        />
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        sx={{
-          background: "#f7f7f7",
-          border: "1px solid #eaeaea",
-          borderTop: 0,
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            padding: "2px",
-            paddingRight: "5px",
-            justifyContent: "flex-end",
-            "& button": {
-              height: "26px",
-              width: "26px",
-              padding: "2px",
-              " .min-icon": {
-                marginLeft: "0",
-              },
-            },
-          }}
-        >
+    <CodeEditor
+      value={value}
+      onChange={(value) => onChange(value)}
+      mode={mode}
+      tooltip={tooltip}
+      editorHeight={editorHeight}
+      label={label}
+      helpTools={
+        <Fragment>
           <TooltipWrapper tooltip={"Copy to Clipboard"}>
             <CopyToClipboard text={value}>
               <Button
@@ -134,10 +57,10 @@ const CodeMirrorWrapper = ({
               />
             </CopyToClipboard>
           </TooltipWrapper>
-        </Box>
-      </Grid>
-    </React.Fragment>
+        </Fragment>
+      }
+    />
   );
 };
 
-export default withStyles(styles)(CodeMirrorWrapper);
+export default CodeMirrorWrapper;
