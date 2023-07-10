7243dbeaa3f650ebd3ae2e6f868ee99cc866e09f
prakashsvmx <prakash@minio.io>
2023-07-10 16:42:23 +0530

    revert a line

--- portal-ui/src/screens/Console/Buckets/ListBuckets/Objects/ListObjects/types.tsx
+++ portal-ui/src/screens/Console/Buckets/ListBuckets/Objects/ListObjects/types.tsx
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { BucketObject } from "api/consoleApi";
+import { IFileInfo } from "../ObjectDetails/types";
 
 export interface BucketObjectItem {
   name: string;
