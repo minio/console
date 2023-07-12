076c526f9982fc5be6f53670e9b7d4ea54cf6fb5
Jillian Inapurapu <jillii@Jillians-MBP.attlocal.net>
2023-07-11 17:08:34 -0700

    Prettier

--- portal-ui/src/screens/Console/Configurations/SiteReplication/SiteReplication.tsx
+++ portal-ui/src/screens/Console/Configurations/SiteReplication/SiteReplication.tsx
@@ -45,7 +45,6 @@ import TooltipWrapper from "../../Common/TooltipWrapper/TooltipWrapper";
 import PageHeaderWrapper from "../../Common/PageHeaderWrapper/PageHeaderWrapper";
 import HelpMenu from "../../HelpMenu";
 
-
 export type ReplicationSite = {
   deploymentID: string;
   endpoint: string;
