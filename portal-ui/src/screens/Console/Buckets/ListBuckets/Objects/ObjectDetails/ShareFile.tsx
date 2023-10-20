57f91972efa2d6f6c78fde19b5997fe9ebc6e2e3
cesnietor <>
2023-10-20 10:36:14 -0700

    Updated docs on how presigned url works

--- portal-ui/src/screens/Console/Buckets/ListBuckets/Objects/ObjectDetails/ShareFile.tsx
+++ portal-ui/src/screens/Console/Buckets/ListBuckets/Objects/ObjectDetails/ShareFile.tsx
@@ -208,9 +208,12 @@ const ShareFile = ({
                 fontWeight: 400,
               }}
             >
-              This is a temporary URL with integrated access credentials for
-              sharing objects valid for up to{" "}
-              {formatTime(maxshareLinkExpTimeVal)}.
+              With MinIO Console, you can create a temporary URL with integrated
+              access credentials for sharing objects valid for up to:{" "}
+              {formatTime(maxshareLinkExpTimeVal)}. To create a presigned URL
+              with a longer time interval please update
+              MINIO_BROWSER_SESSION_DURATION environment variable on your server
+              MinIO or use MinIO Client.
               <br />
               <br />
               The temporary URL expires after the configured time limit.
