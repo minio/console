2a470a826ce7b1d88418ffdf223a63f52cb75152
Jillian Inapurapu <jillii@Jillians-MBP.attlocal.net>
2024-01-24 12:40:49 -0800

    WIP URL validation

--- web-app/src/screens/Console/Buckets/BucketDetails/AddBucketReplication.tsx
+++ web-app/src/screens/Console/Buckets/BucketDetails/AddBucketReplication.tsx
@@ -147,9 +147,22 @@ const AddBucketReplication = () => {
       });
   };
 
-  let validURL = new RegExp(/(?<=^|\s)(\w*-?\w+\.[a-z]{2,}\S*)/);
+  //let validURL = new RegExp(
+  //   /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i,
+  // );
+  //const checkValidURL = (testURL: string) => {
+  //console.log("Tested URL: ", validURL.test(testURL));
+  //return validURL.test(testURL);
   const checkValidURL = (testURL: string) => {
-    return validURL.test(testURL);
+    const endURL = testURL.startsWith("https://" || "http://")
+      ? testURL
+      : `${useTLS ? "https://" : "http://"}${testURL}`;
+    try {
+      new URL(endURL);
+      return true;
+    } catch (err) {
+      return false;
+    }
   };
 
   useEffect(() => {
@@ -161,6 +174,7 @@ const AddBucketReplication = () => {
       setValidated(true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [targetURL, accessKey, secretKey, targetBucket]);
+
   useEffect(() => {
     if (
       (validated && accessKey.length < 3) ||
