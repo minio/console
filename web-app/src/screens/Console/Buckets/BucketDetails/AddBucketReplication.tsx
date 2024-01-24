6387a42fa84e4d015f1b57f8e46af67e1e435c9a
Jillian Inapurapu <jillii@Jillians-MBP.attlocal.net>
2024-01-24 13:23:29 -0800

    Simplified field validation

--- web-app/src/screens/Console/Buckets/BucketDetails/AddBucketReplication.tsx
+++ web-app/src/screens/Console/Buckets/BucketDetails/AddBucketReplication.tsx
@@ -125,10 +125,10 @@ const AddBucketReplication = () => {
             dispatch(
               setErrorSnackMessage({
                 errorMessage: itemVal.errorString,
-                detailedError: "",
+                detailedError: "There was an error",
               }),
             );
-            navigate(backLink);
+            // navigate(backLink);
             return;
           }
           navigate(backLink);
@@ -142,27 +142,10 @@ const AddBucketReplication = () => {
         );
       })
       .catch((err) => {
+        console.log("this is an error!");
         setAddLoading(false);
         dispatch(setErrorSnackMessage(errorToHandler(err.error)));
       });
-  };
-
-  //let validURL = new RegExp(
-  //   /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i,
-  // );
-  //const checkValidURL = (testURL: string) => {
-  //console.log("Tested URL: ", validURL.test(testURL));
-  //return validURL.test(testURL);
-  const checkValidURL = (testURL: string) => {
-    const endURL = testURL.startsWith("https://" || "http://")
-      ? testURL
-      : `${useTLS ? "https://" : "http://"}${testURL}`;
-    try {
-      new URL(endURL);
-      return true;
-    } catch (err) {
-      return false;
-    }
   };
 
   useEffect(() => {
@@ -170,22 +153,21 @@ const AddBucketReplication = () => {
       accessKey.length >= 3 &&
       secretKey.length >= 8 &&
       targetBucket.length >= 3 &&
-      checkValidURL(targetURL) &&
+      targetURL.length > 0 &&
       setValidated(true);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [targetURL, accessKey, secretKey, targetBucket]);
+  }, [targetURL, accessKey, secretKey, targetBucket, validated]);
 
   useEffect(() => {
     if (
-      (validated && accessKey.length < 3) ||
-      secretKey.length < 8 ||
-      targetBucket.length < 3 ||
-      !checkValidURL(targetURL)
+      validated &&
+      (accessKey.length < 3 ||
+        secretKey.length < 8 ||
+        targetBucket.length < 3 ||
+        targetURL.length < 1)
     ) {
       setValidated(false);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [targetURL, accessKey, secretKey, targetBucket]);
+  }, [targetURL, accessKey, secretKey, targetBucket, validated]);
 
   return (
     <Fragment>
