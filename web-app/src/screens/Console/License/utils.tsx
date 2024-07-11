57ab6fbeae05addffd6ebb0d61e297a87f72f2f6
Benjamin Perez <benjamin@bexsoft.net>
2024-07-11 01:45:00 -0600

    Updated License page with new design
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

--- web-app/src/screens/Console/License/utils.tsx
+++ web-app/src/screens/Console/License/utils.tsx
@@ -14,443 +14,216 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import LicenseLink from "./LicenseLink";
-import { openFAQModal } from "./licenseSlice";
-import store from "../../../store";
-import FAQModal from "./FAQModal";
-import { Box } from "mds";
+import React from "react";
+import { ApplicationLogo } from "mds";
 
-export const LICENSE_PLANS = {
-  COMMUNITY: "community",
-  STANDARD: "standard",
-  ENTERPRISE: "enterprise",
-  ENTERPRISE_LITE: "enterprise-lite",
-  ENTERPRISE_PLUS: "enterprise-plus",
-};
+export interface LicensePlanOption {
+  planId: string;
+  planName: string;
+  planType: "commercial" | "open-source";
+  planIcon: React.ReactNode;
+  planDescription: React.ReactNode;
+}
 
-type FeatureItem = {
-  label?: any;
-  isHeader?: boolean;
-  style?: any;
-  desc?: any;
-  featureTitleRow?: boolean;
-};
+export interface FeatureElementObject {
+  [name: string]: FeatureItem;
+}
 
-const FeatureLink = ({ text, anchor }: { text: string; anchor: string }) => {
-  return (
-    <a
-      href={`https://min.io/product/subnet?ref=con#${anchor}`}
-      className={"link-text"}
-      target="_blank"
-      rel="noopener "
-      style={{
-        color: "#2781B0",
-      }}
-    >
-      {text}
-    </a>
-  );
-};
+export interface FeatureItem {
+  content: React.ReactNode;
+  isCheck?: boolean;
+}
 
-export const FEATURE_ITEMS: FeatureItem[] = [
+export interface PlansFeatures {
+  featureLabel: string;
+  featurePlans: FeatureElementObject;
+}
+
+export const FEATURE_ITEMS: PlansFeatures[] = [
   {
-    label: "License ",
-    isHeader: true,
-  },
-  {
-    label: "",
-    isHeader: false,
-    style: {
-      height: "400px",
-      verticalAlign: "top",
-      alignItems: "start",
+    featureLabel: "License",
+    featurePlans: {
+      openSource: {
+        content: "Requires AGPLv3 License Compliance",
+      },
+      eosLite: {
+        content: "Commercial License",
+      },
+      eosPlus: {
+        content: "Commercial License",
+      },
     },
   },
   {
-    desc: "Features",
-    featureTitleRow: true,
-  },
-  {
-    desc: "Unit Price",
-  },
-  {
-    desc: () => {
-      return (
-        <FeatureLink
-          anchor={"sa-long-term-support"}
-          text={"Software Release"}
-        />
-      );
+    featureLabel: "Release",
+    featurePlans: {
+      openSource: {
+        content: "Upstream Community Release",
+      },
+      eosLite: {
+        content: "Enterprise Stable Release",
+      },
+      eosPlus: {
+        content: "Enterprise Stable Release",
+      },
     },
   },
   {
-    desc: "SLA",
-  },
-  {
-    desc: "Support",
-  },
-  {
-    desc: "Critical Security and Bug Detection",
-  },
-  {
-    desc: () => {
-      return <FeatureLink anchor={"sa-panic-button"} text={"Panic Button"} />;
+    featureLabel: "Additional Features",
+    featurePlans: {
+      openSource: {
+        content: "None",
+      },
+      eosLite: {
+        content:
+          "Global Console, Observability, Cache, Data Firewall, Key Management Server Catalog",
+      },
+      eosPlus: {
+        content:
+          "Global Console, Observability, Cache, Data Firewall, Key Management Server Catalog",
+      },
     },
   },
   {
-    desc: () => {
-      return (
-        <FeatureLink anchor={"sa-healthcheck"} text={"Health Diagnostics"} />
-      );
+    featureLabel: "Long Term Release Support",
+    featurePlans: {
+      openSource: {
+        content: "None",
+      },
+      eosLite: {
+        content: "1 year LTS",
+      },
+      eosPlus: {
+        content: "5 years LTS",
+      },
     },
   },
   {
-    desc: "Annual Architecture Review",
+    featureLabel: "Support SLA",
+    featurePlans: {
+      openSource: {
+        content: "No SLA",
+      },
+      eosLite: {
+        content: "Next Business Day SLA",
+      },
+      eosPlus: {
+        content: "Less than 4 Hour SLA",
+      },
+    },
   },
   {
-    desc: "Annual Performance Review",
+    featureLabel: "Panic button",
+    featurePlans: {
+      openSource: {
+        content: "None",
+      },
+      eosLite: {
+        content: "1 Panic Button Per Year",
+      },
+      eosPlus: {
+        content: "Unlimited Panic Buttons Per Year",
+      },
+    },
   },
   {
-    desc: "Indemnification",
+    featureLabel:
+      "Call Home Diagnostics, Health Check, Performance Benchmark, Security and Critical Vulnerabilities Notifications",
+    featurePlans: {
+      openSource: {
+        content: "",
+      },
+      eosLite: {
+        content: "",
+        isCheck: true,
+      },
+      eosPlus: {
+        content: "",
+        isCheck: true,
+      },
+    },
   },
   {
-    desc: "Security and Policy Review",
+    featureLabel: "Indemnification",
+    featurePlans: {
+      openSource: {
+        content: "",
+      },
+      eosLite: {
+        content: "",
+      },
+      eosPlus: {
+        content: "",
+        isCheck: true,
+      },
+    },
+  },
+  {
+    featureLabel: "Annual Review of Architecture, Performance and Security",
+    featurePlans: {
+      openSource: {
+        content: "",
+      },
+      eosLite: {
+        content: "",
+      },
+      eosPlus: {
+        content: "",
+        isCheck: true,
+      },
+    },
   },
 ];
 
-export const COMMUNITY_PLAN_FEATURES = [
+export const LICENSE_PLANS_INFORMATION: LicensePlanOption[] = [
   {
-    label: "Community",
-    isHeader: true,
-    style: {
-      borderBottom: 0,
-    },
-  },
-  {
-    label: () => {
-      return (
-        <Box
-          sx={{
-            textAlign: "left",
-          }}
-        >
-          <span>
-            Designed for developers who are building open source applications in
-            compliance with the <LicenseLink /> license, MinIO Trademarks and
-            are able to self support themselves. It is fully featured. If you
-            distribute, host or create derivative works of the MinIO software
-            over the network, the <LicenseLink /> license requires that you also
-            distribute the complete, corresponding source code of the combined
-            work under the same <LicenseLink /> license. This requirement
-            applies whether or not you modified MinIO.
-            <br />
-            <br />
-            <span
-              className="link-text"
-              onClick={() => {
-                store.dispatch(openFAQModal());
-              }}
-            >
-              Compliance FAQ
-            </span>
-            <FAQModal />
-          </span>
-        </Box>
-      );
-    },
-    isHeader: false,
-    style: {
-      height: "400px",
-      borderBottom: 0,
-    },
-  },
-  {
-    id: "com_feat_title",
-    featureTitleRow: true,
-  },
-  {
-    id: "com_license_cost",
-  },
-  {
-    id: "com_release",
-    label: "Upstream",
-  },
-  {
-    id: "com_sla",
-    label: "No SLA",
-  },
-  {
-    id: "com_support",
-    label: "Community:",
-    detail: "Slack + GitHub",
-  },
-  {
-    id: "com_security",
-    label: "Self",
-  },
-  {
-    id: "com_panic",
-    xsLabel: "N/A",
-  },
-  {
-    id: "com_diag",
-    xsLabel: "N/A",
-  },
-  {
-    id: "com_arch",
-    xsLabel: "N/A",
-  },
-  {
-    id: "com_perf",
-    xsLabel: "N/A",
-  },
-  {
-    id: "com_indemnity",
-    xsLabel: "N/A",
-  },
-  {
-    id: "com_sec_policy",
-    xsLabel: "N/A",
-  },
-];
-
-export const STANDARD_PLAN_FEATURES = [
-  {
-    label: "Standard",
-    isHeader: true,
-    style: {
-      borderBottom: 0,
-    },
-  },
-  {
-    isHeader: false,
-    label: () => {
-      return (
-        <Box
-          sx={{
-            marginTop: "-85px",
-            textAlign: "left",
-          }}
-        >
-          <span>
-            Designed for customers who require a commercial license and can
-            mostly self-support but want the peace of mind that comes with the
-            MinIO Subscription Network’s suite of operational capabilities and
-            direct-to-engineer interaction. The Standard version is fully
-            featured but with SLA limitations. <br /> <br /> To learn more about
-            the MinIO Subscription Network
-          </span>{" "}
-          <a
-            href="https://min.io/product/subnet?ref=con"
-            className={"link-text"}
-            target="_blank"
-            rel="noopener"
-          >
-            click here
-          </a>
-          .
-        </Box>
-      );
-    },
-    style: {
-      height: "400px",
-      borderBottom: 0,
-    },
-  },
-  {
-    id: "std_feat_title",
-    featureTitleRow: true,
-  },
-  {
-    id: "std_license_cost",
-    label: () => (
-      <Box
-        sx={{
-          fontSize: "16px",
-          fontWeight: 600,
-        }}
-      >
-        $10 per TiB per month
-      </Box>
+    planId: "openSource",
+    planName: "Open Source",
+    planType: "open-source",
+    planIcon: (
+      <ApplicationLogo applicationName={"console"} subVariant={"AGPL"} />
     ),
-    detail: () => (
-      <Box
-        sx={{
-          fontSize: "14px",
-          fontWeight: 400,
-          marginBottom: "5px",
-        }}
-      >
-        (Minimum of 200TiB)
-      </Box>
+    planDescription: (
+      <span>
+        Designed for developers who are building open source applications in
+        compliance with the GNU AGPL v3 license which requires developers to
+        distribute their code under the same AGPL v3 license when they
+        distribute, host or modify MinIO.
+      </span>
     ),
   },
   {
-    id: "std_release",
-    label: "1 Year Long Term Support",
-  },
-  {
-    id: "std_sla",
-    label: "<48 Hours",
-    detail: "(Local Business Hours)",
-  },
-  {
-    id: "std_support",
-    label: "L4 Direct Engineering",
-    detail: "support via SUBNET",
-  },
-  {
-    id: "std_security",
-    label: "Continuous Scan and Alert",
-  },
-  {
-    id: "std_panic",
-    label: "1 Per year",
-  },
-  {
-    id: "std_diag",
-    label: "24/7/365",
-  },
-  {
-    id: "std_arch",
-    xsLabel: "N/A",
-  },
-  {
-    id: "std_perf",
-    xsLabel: "N/A",
-  },
-  {
-    id: "std_indemnity",
-    xsLabel: "N/A",
-  },
-  {
-    id: "std_sec_policy",
-    xsLabel: "N/A",
-  },
-];
-
-export const ENTERPRISE_PLAN_FEATURES = [
-  {
-    label: "Enterprise",
-    isHeader: true,
-    style: {
-      borderBottom: 0,
-    },
-  },
-  {
-    isHeader: false,
-    label: () => {
-      return (
-        <Box
-          sx={{
-            marginTop: "-135px",
-            textAlign: "left",
-          }}
-        >
-          <span>
-            Designed for mission critical environments where both a license and
-            strict SLAs are required. The Enterprise version is fully featured
-            but comes with additional capabilities. <br /> <br /> To learn more
-            about the MinIO Subscription Network
-          </span>{" "}
-          <a
-            href="https://min.io/product/subnet?ref=con"
-            className={"link-text"}
-            target="_blank"
-            rel="noopener"
-          >
-            click here
-          </a>
-          .
-        </Box>
-      );
-    },
-    style: {
-      height: "400px",
-      borderBottom: 0,
-    },
-  },
-  {
-    id: "end_feat_title",
-    featureTitleRow: true,
-  },
-  {
-    id: "ent_license_cost",
-    label: () => (
-      <Box
-        sx={{
-          fontSize: "16px",
-          fontWeight: 600,
-        }}
-      >
-        $20 per TiB per month
-      </Box>
+    planId: "eosLite",
+    planName: "Enterprise Lite",
+    planType: "commercial",
+    planIcon: (
+      <ApplicationLogo applicationName={"minio"} subVariant={"enterpriseos"} />
     ),
-    detail: () => (
-      <Box
-        sx={{
-          fontSize: "14px",
-          fontWeight: 400,
-          marginBottom: "5px",
-        }}
-      >
-        (Minimum of 100TiB)
-      </Box>
+    planDescription: (
+      <span>
+        Designed for customers who require a commercial license and can mostly
+        self-support but want the peace of mind that comes with an
+        engineer-backend SLA, additional features and operational capabilities.
+      </span>
     ),
   },
   {
-    id: "ent_release",
-    label: "5 Years Long Term Support",
-  },
-  {
-    id: "ent_sla",
-    label: "<1 hour",
-  },
-  {
-    id: "ent_support",
-    label: "L4 Direct Engineering support via",
-    detail: "SUBNET, Phone, Web Conference",
-  },
-  {
-    id: "ent_security",
-    label: "Continuous Scan and Alert",
-  },
-  {
-    id: "ent_panic",
-    label: "Unlimited",
-  },
-  {
-    id: "ent_diag",
-    label: "24/7/365",
-  },
-  {
-    id: "ent_arch",
-    yesIcon: true,
-  },
-  {
-    id: "ent_perf",
-    yesIcon: true,
-  },
-  {
-    id: "ent_indemnity",
-    yesIcon: true,
-  },
-  {
-    id: "ent_sec_policy",
-    yesIcon: true,
+    planId: "eosPlus",
+    planName: "Enterprise Plus",
+    planType: "commercial",
+    planIcon: (
+      <ApplicationLogo applicationName={"minio"} subVariant={"enterpriseos"} />
+    ),
+    planDescription: (
+      <span>
+        Designed for customers where a commercial license and the
+        strictest,engineer-backed SLA are required. The Plus tiers offers
+        additional features and operational capabilities, more interaction
+        options and more enterprise deliverables.
+      </span>
+    ),
   },
 ];
-
-export const PAID_PLANS = [
-  LICENSE_PLANS.STANDARD,
-  LICENSE_PLANS.ENTERPRISE,
-  LICENSE_PLANS.ENTERPRISE_LITE,
-  LICENSE_PLANS.ENTERPRISE_PLUS,
-];
-
-export const getRenderValue = (val: any) => {
-  return typeof val === "function" ? val() : val;
-};
 
 export const LICENSE_CONSENT_STORE_KEY = "agpl_minio_license_consent";
 export const setLicenseConsent = () => {
